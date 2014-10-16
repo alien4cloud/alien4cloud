1b9bd0d188c71f71c574066659f4f98fd19afc53
Luc Boutier <luc.boutier@me.com>
2014-10-16 09:38:11 +0200

    [ALIEN-453] Recreate artifact repository if removed.

--- alien4cloud-core/src/main/java/alien4cloud/component/repository/ArtifactLocalRepository.java
+++ alien4cloud-core/src/main/java/alien4cloud/component/repository/ArtifactLocalRepository.java
@@ -1,6 +1,7 @@
 package alien4cloud.component.repository;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -8,6 +9,8 @@ import java.nio.file.Paths;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import alien4cloud.component.repository.exception.RepositoryIOException;
 
 @Component
 public class ArtifactLocalRepository extends AbstractLocalRepository {
@@ -19,12 +22,40 @@ public class ArtifactLocalRepository extends AbstractLocalRepository {
         return this.repositoryPath;
     }
 
+    @Override
+    public void storeFile(String id, InputStream data) {
+        try {
+            super.storeFile(id, data);
+        } catch (RepositoryIOException e) {
+            ensureRepositoryExists();
+            super.storeFile(id, data);
+        }
+    }
+
+    @Override
+    public String storeFile(InputStream data) {
+        try {
+            return super.storeFile(data);
+        } catch (RepositoryIOException e) {
+            ensureRepositoryExists();
+            return super.storeFile(data);
+        }
+    }
+
     @Required
     @Value("${directories.alien}/${directories.artifact_repository}")
     public void setRepositoryPath(String path) throws IOException {
         this.repositoryPath = Paths.get(path).toAbsolutePath();
+        ensureRepositoryExists();
+    }
+
+    private void ensureRepositoryExists() {
         if (!Files.exists(repositoryPath)) {
-            Files.createDirectories(repositoryPath);
+            try {
+                Files.createDirectories(repositoryPath);
+            } catch (IOException e) {
+                throw new RepositoryIOException("Fails to create artifact repository at " + repositoryPath.toString());
+            }
         }
     }
 }
