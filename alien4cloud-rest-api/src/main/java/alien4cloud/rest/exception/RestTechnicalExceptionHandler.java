501eeba86a8de666683be6699f9edfbf296fa288
vuminhkh <minh.khang.vu.mkv@gmail.com>
2014-10-27 14:29:29 +0100

    ALIEN-462 : As a PaaS Provider developper I want to receive notifications when the list of compute template for a given cloud changes. ALIEN-458 : As a USER I want the cloud resource matching to be persisted in the deployment setup. ALIEN-459 : As a USER I want the cloud deployment properties to be persisted within the deployment setup ALIEN-460 : As a USER I want the cloud deployment properties to be persisted within the deployment setup Minor bug correction

--- alien4cloud-rest-api/src/main/java/alien4cloud/rest/exception/RestTechnicalExceptionHandler.java
+++ alien4cloud-rest-api/src/main/java/alien4cloud/rest/exception/RestTechnicalExceptionHandler.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
-import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.BindingResult;
@@ -16,8 +14,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import com.google.common.collect.Lists;
+
 import alien4cloud.component.repository.exception.RepositoryTechnicalException;
 import alien4cloud.exception.AlreadyExistException;
+import alien4cloud.exception.DeleteReferencedObjectException;
 import alien4cloud.exception.IndexingServiceException;
 import alien4cloud.exception.InvalidArgumentException;
 import alien4cloud.exception.NotFoundException;
@@ -32,12 +33,11 @@ import alien4cloud.rest.model.RestResponse;
 import alien4cloud.rest.model.RestResponseBuilder;
 import alien4cloud.security.Alien4CloudAccessDeniedHandler;
 import alien4cloud.tosca.container.exception.CSARTechnicalException;
-
-import com.google.common.collect.Lists;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * All technical (runtime) exception handler goes here. It's unexpected exception and is in general back-end exception or bug in our code
- * 
+ *
  * @author mkv
  */
 @Slf4j
@@ -51,8 +51,17 @@ public class RestTechnicalExceptionHandler {
     @ResponseStatus(HttpStatus.CONFLICT)
     @ResponseBody
     public RestResponse<Void> processAlreadyExist(AlreadyExistException e) {
-        return RestResponseBuilder.<Void> builder()
+        return RestResponseBuilder.<Void>builder()
                 .error(RestErrorBuilder.builder(RestErrorCode.ALREADY_EXIST_ERROR).message("The posted object already exist.").build()).build();
+    }
+
+    @ExceptionHandler(DeleteReferencedObjectException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    @ResponseBody
+    public RestResponse<Void> processDeleteReferencedObject(DeleteReferencedObjectException e) {
+        log.error("Object is still referenced and cannot be deleted", e);
+        return RestResponseBuilder.<Void>builder()
+                .error(RestErrorBuilder.builder(RestErrorCode.DELETE_REFERENCED_OBJECT_ERROR).message("The deleted object is still referenced.").build()).build();
     }
 
     @ExceptionHandler(value = MissingPluginException.class)
@@ -61,7 +70,7 @@ public class RestTechnicalExceptionHandler {
     public RestResponse<Void> missingPluginExceptionHandler(MissingPluginException e) {
         log.error("PaaS provider plugin cannot be found while used on a cloud, this should not happens.", e);
         return RestResponseBuilder
-                .<Void> builder()
+                .<Void>builder()
                 .error(RestErrorBuilder.builder(RestErrorCode.MISSING_PLUGIN_ERROR)
                         .message("The cloud plugin cannot be found. Make sure that the plugin is installed and enabled.").build()).build();
     }
@@ -71,7 +80,7 @@ public class RestTechnicalExceptionHandler {
     @ResponseBody
     public RestResponse<Void> invalidArgumentErrorHandler(InvalidArgumentException e) {
         log.error("Method argument is invalid", e);
-        return RestResponseBuilder.<Void> builder()
+        return RestResponseBuilder.<Void>builder()
                 .error(RestErrorBuilder.builder(RestErrorCode.ILLEGAL_PARAMETER).message("Method argument is invalid " + e.getMessage()).build()).build();
     }
 
@@ -84,7 +93,7 @@ public class RestTechnicalExceptionHandler {
         for (FieldError fieldError : result.getFieldErrors()) {
             errors.add(new FieldErrorDTO(fieldError.getField(), fieldError.getCode()));
         }
-        return RestResponseBuilder.<FieldErrorDTO[]> builder().data(errors.toArray(new FieldErrorDTO[errors.size()]))
+        return RestResponseBuilder.<FieldErrorDTO[]>builder().data(errors.toArray(new FieldErrorDTO[errors.size()]))
                 .error(RestErrorBuilder.builder(RestErrorCode.ILLEGAL_PARAMETER).message("Method argument is invalid " + e.getMessage()).build()).build();
     }
 
@@ -94,7 +103,7 @@ public class RestTechnicalExceptionHandler {
     public RestResponse<Void> indexingServiceErrorHandler(IndexingServiceException e) {
         log.error("Indexing service has encoutered unexpected error", e);
         return RestResponseBuilder
-                .<Void> builder()
+                .<Void>builder()
                 .error(RestErrorBuilder.builder(RestErrorCode.INDEXING_SERVICE_ERROR)
                         .message("Indexing service has encoutered unexpected error " + e.getMessage()).build()).build();
     }
@@ -105,7 +114,7 @@ public class RestTechnicalExceptionHandler {
     public RestResponse<Void> repositryServiceErrorHandler(RepositoryTechnicalException e) {
         log.error("Repository service has encoutered unexpected error", e);
         return RestResponseBuilder
-                .<Void> builder()
+                .<Void>builder()
                 .error(RestErrorBuilder.builder(RestErrorCode.REPOSITORY_SERVICE_ERROR)
                         .message("Repository service has encoutered unexpected error " + e.getMessage()).build()).build();
     }
@@ -116,7 +125,7 @@ public class RestTechnicalExceptionHandler {
     public RestResponse<Void> csarErrorHandler(CSARTechnicalException e) {
         log.error("CSAR processing has encoutered unexpected error", e);
         return RestResponseBuilder
-                .<Void> builder()
+                .<Void>builder()
                 .error(RestErrorBuilder.builder(RestErrorCode.CSAR_PARSING_ERROR).message("CSAR processing has encoutered unexpected error " + e.getMessage())
                         .build()).build();
     }
@@ -126,7 +135,7 @@ public class RestTechnicalExceptionHandler {
     @ResponseBody
     public RestResponse<Void> imageUploadErrorHandler(ImageUploadException e) {
         log.error("Image upload error", e);
-        return RestResponseBuilder.<Void> builder()
+        return RestResponseBuilder.<Void>builder()
                 .error(RestErrorBuilder.builder(RestErrorCode.IMAGE_UPLOAD_ERROR).message("Image upload error " + e.getMessage()).build()).build();
     }
 
@@ -135,7 +144,7 @@ public class RestTechnicalExceptionHandler {
     @ResponseBody
     public RestResponse<Void> notFoundErrorHandler(NotFoundException e) {
         log.error("Something not found", e);
-        return RestResponseBuilder.<Void> builder().error(RestErrorBuilder.builder(RestErrorCode.NOT_FOUND_ERROR).message(e.getMessage()).build()).build();
+        return RestResponseBuilder.<Void>builder().error(RestErrorBuilder.builder(RestErrorCode.NOT_FOUND_ERROR).message(e.getMessage()).build()).build();
     }
 
     @ExceptionHandler(value = VersionConflictException.class)
@@ -143,7 +152,7 @@ public class RestTechnicalExceptionHandler {
     @ResponseBody
     public RestResponse<Void> versionConflictHandler(VersionConflictException e) {
         log.error("Version conflict", e);
-        return RestResponseBuilder.<Void> builder().error(RestErrorBuilder.builder(RestErrorCode.VERSION_CONFLICT_ERROR).message(e.getMessage()).build())
+        return RestResponseBuilder.<Void>builder().error(RestErrorBuilder.builder(RestErrorCode.VERSION_CONFLICT_ERROR).message(e.getMessage()).build())
                 .build();
     }
 
@@ -153,7 +162,7 @@ public class RestTechnicalExceptionHandler {
     public RestResponse<Void> paaSDeploymentErrorHandler(PaaSDeploymentException e) {
         log.error("Error in PaaS Deployment", e);
         return RestResponseBuilder
-                .<Void> builder()
+                .<Void>builder()
                 .error(RestErrorBuilder.builder(RestErrorCode.APPLICATION_DEPLOYMENT_ERROR).message("Application cannot be deployed " + e.getMessage()).build())
                 .build();
     }
@@ -164,7 +173,7 @@ public class RestTechnicalExceptionHandler {
     public RestResponse<Void> paaSUndeploymentErrorHandler(PaaSUndeploymentException e) {
         log.error("Error in UnDeployment", e);
         return RestResponseBuilder
-                .<Void> builder()
+                .<Void>builder()
                 .error(RestErrorBuilder.builder(RestErrorCode.APPLICATION_UNDEPLOYMENT_ERROR).message("Application cannot be undeployed " + e.getMessage())
                         .build()).build();
     }
@@ -181,7 +190,7 @@ public class RestTechnicalExceptionHandler {
     @ResponseBody
     public RestResponse<Void> catchAllErrorHandler(Exception e) {
         log.error("Uncategorized error", e);
-        return RestResponseBuilder.<Void> builder()
+        return RestResponseBuilder.<Void>builder()
                 .error(RestErrorBuilder.builder(RestErrorCode.UNCATEGORIZED_ERROR).message("Uncategorized error " + e.getMessage()).build()).build();
     }
 }
