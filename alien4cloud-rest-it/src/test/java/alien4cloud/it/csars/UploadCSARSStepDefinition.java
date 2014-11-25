15a82f5e27cc8e65376089d52c6f83ae7bc26ed8
Luc Boutier <luc.boutier@me.com>
2014-11-25 11:16:07 +0100

    Ensure that test archives are built even when IT test are not launched (this is needed to start UI tests but IT tests)

--- alien4cloud-rest-it/src/test/java/alien4cloud/it/csars/UploadCSARSStepDefinition.java
+++ alien4cloud-rest-it/src/test/java/alien4cloud/it/csars/UploadCSARSStepDefinition.java
@@ -1,11 +1,8 @@
 package alien4cloud.it.csars;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Map;
-import java.util.Map.Entry;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -14,70 +11,22 @@ import org.junit.Assert;
 import alien4cloud.component.model.IndexedNodeType;
 import alien4cloud.it.Context;
 import alien4cloud.it.common.CommonStepDefinitions;
+import alien4cloud.it.setup.TestDataRegistry;
 import alien4cloud.rest.model.RestResponse;
 import alien4cloud.rest.utils.JsonUtil;
 import alien4cloud.tosca.parser.ParsingResult;
-import alien4cloud.utils.FileUtil;
-
-import com.google.common.collect.Maps;
-
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 
 @Slf4j
 public class UploadCSARSStepDefinition {
-    private static final Path ARCHIVES_TARGET_PATH = Paths.get("../target/it-artifacts/zipped/");
-
-    private static final Map<String, Path> conditionToPath = Maps.newHashMap();
-    static {
-        try {
-            FileUtil.delete(ARCHIVES_TARGET_PATH);
-        } catch (IOException e) {
-            log.error("Failed to delete zipped archives repository.", e);
-        }
-        // Zip all test archive folders so they are ready to be uploaded.
-        conditionToPath.put("tosca base types 1.0", Paths.get("src/test/resources/data/csars/tosca-base-types-1.0"));
-        conditionToPath.put("tosca base types 2.0", Paths.get("src/test/resources/data/csars/tosca-base-types-2.0"));
-        conditionToPath.put("tosca base types 3.0", Paths.get("src/test/resources/data/csars/tosca-base-types-3.0"));
-        conditionToPath.put("sample java types 1.0", Paths.get("src/test/resources/data/csars/sample/java-types-1.0"));
-        conditionToPath.put("sample java types 2.0", Paths.get("src/test/resources/data/csars/sample/java-types-2.0"));
-        conditionToPath.put("sample java types 3.0", Paths.get("src/test/resources/data/csars/sample/java-types-3.0"));
-        conditionToPath.put("ubuntu types 0.1", Paths.get("src/test/resources/data/csars/sample/ubuntu-types-0.1"));
-        conditionToPath.put("sample apache lb types 0.1", Paths.get("src/test/resources/data/csars/sample/apache-lb-types-0.1"));
-        conditionToPath.put("constraints", Paths.get("src/test/resources/data/csars/definition/constraints"));
-        conditionToPath.put("invalid (definition file not found)", Paths.get("src/test/resources/data/csars/definition/missing"));
-        conditionToPath.put("invalid (definition file is not valid yaml file)", Paths.get("src/test/resources/data/csars/definition/erroneous"));
-        conditionToPath.put("invalid (definition file's declaration duplicated)", Paths.get("src/test/resources/data/csars/definition/duplicated"));
-        conditionToPath.put("invalid (ALIEN-META.yaml not found)", Paths.get("src/test/resources/data/csars/metadata/missing"));
-        conditionToPath.put("invalid (ALIEN-META.yaml invalid)", Paths.get("src/test/resources/data/csars/metadata/erroneous"));
-        conditionToPath.put("invalid (ALIEN-META.yaml fail validation)", Paths.get("src/test/resources/data/csars/metadata/validationFailure"));
-        conditionToPath.put("invalid (icon not found)", Paths.get("src/test/resources/data/csars/icon/missing"));
-        conditionToPath.put("invalid (icon invalid)", Paths.get("src/test/resources/data/csars/icon/erroneous"));
-        conditionToPath.put("snapshot", Paths.get("src/test/resources/data/csars/snapshot"));
-        conditionToPath.put("relationship test types", Paths.get("src/test/resources/data/csars/relationship-test-types"));
-        conditionToPath.put("valid-csar-with-test", Paths.get("src/test/resources/data/csars/snapshot-test/snapshot-test-valid"));
-        conditionToPath.put("csar-test-no-topology", Paths.get("src/test/resources/data/csars/snapshot-test/missing-topology-yaml"));
-
-        for (Entry<String, Path> entry : conditionToPath.entrySet()) {
-            try {
-                Path targetPath = ARCHIVES_TARGET_PATH.resolve(entry.getValue().getFileName() + ".csar");
-                FileUtil.zip(entry.getValue(), targetPath);
-                conditionToPath.put(entry.getKey(), targetPath);
-            } catch (IOException e) {
-                throw new RuntimeException("Failed to zip archive for tests.", e);
-            }
-        }
-
-        // test uploading an unzipped file (do not zip it)
-        conditionToPath.put("unzipped", Paths.get("src/test/resources/alien/rest/csars/upload.feature"));
-    }
 
     private Path csarPath = Paths.get(Context.getInstance().getTmpDirectory().toString(), "csar.csar");
     private CommonStepDefinitions commonSteps = new CommonStepDefinitions();
 
     @Given("^I upload the archive \"([^\"]*)\"$")
     public void uploadArchive(String key) throws Throwable {
-        Path archive = conditionToPath.get(key);
+        Path archive = TestDataRegistry.CONDITION_TO_PATH.get(key);
         Context.getInstance().registerRestResponse(Context.getRestClientInstance().postMultipart("/rest/csars", "file", Files.newInputStream(archive)));
     }
 
---
+++ alien4cloud-rest-it/src/test/java/alien4cloud/it/setup/PrepareTestData.java
@@ -0,0 +1,32 @@
+package alien4cloud.it.setup;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+
+import lombok.extern.slf4j.Slf4j;
+import alien4cloud.utils.FileUtil;
+
+/**
+ * Simple class that package the test archives as zip.
+ */
+@Slf4j
+public class PrepareTestData {
+    public static String ARCHIVES_TARGET_PATH = "../target/it-artifacts/zipped/";
+    public static String BASEDIR = "";
+
+    public static void main(String[] args) {
+        String baseDir = args[0];
+        ARCHIVES_TARGET_PATH = baseDir + "/" + ARCHIVES_TARGET_PATH;
+        BASEDIR = baseDir + "/";
+        System.out.println("main stuff");
+        for (Map.Entry<Path, Path> entry : TestDataRegistry.FOLDER_TO_ZIP.entrySet()) {
+            try {
+                FileUtil.zip(entry.getKey(), entry.getValue());
+            } catch (IOException e) {
+                throw new RuntimeException("Failed to zip archive for tests.", e);
+            }
+        }
+    }
+}
