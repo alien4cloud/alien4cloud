2531ca3a9837ce742a26c9573238004ee2ca82f6
vuminhkh <minh.khang.vu.mkv@gmail.com>
2014-11-27 16:25:37 +0100

    ALIEN-510 As a User I want Alien 4 Cloud's cloudify2 driver to support Network Test Rest

--- alien4cloud-rest-it/src/test/java/alien4cloud/it/cloud/CloudNetworkStepDefinitions.java
+++ alien4cloud-rest-it/src/test/java/alien4cloud/it/cloud/CloudNetworkStepDefinitions.java
@@ -3,13 +3,17 @@ package alien4cloud.it.cloud;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
 import org.junit.Assert;
 
 import alien4cloud.it.Context;
+import alien4cloud.model.cloud.MatchedNetwork;
 import alien4cloud.model.cloud.Network;
 import alien4cloud.rest.cloud.CloudDTO;
 import alien4cloud.rest.utils.JsonUtil;
 
+import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 
 import cucumber.api.DataTable;
@@ -21,7 +25,7 @@ public class CloudNetworkStepDefinitions {
 
     @When("^I add the network with name \"([^\"]*)\" and CIDR \"([^\"]*)\" and IP version (\\d+) and gateway \"([^\"]*)\" to the cloud \"([^\"]*)\"$")
     public void I_add_the_network_with_name_and_CIDR_and_IP_version_and_gateway_to_the_cloud(String name, String cidr, int ipVersion, String gateWay,
-                                                                                             String cloudName) throws Throwable {
+            String cloudName) throws Throwable {
         String cloudId = Context.getInstance().getCloudId(cloudName);
         Network network = new Network();
         network.setNetworkName(name);
@@ -70,5 +74,53 @@ public class CloudNetworkStepDefinitions {
         String cloudId = Context.getInstance().getCloudId(cloudName);
         CloudDTO cloudDTO = JsonUtil.read(Context.getRestClientInstance().get("/rest/clouds/" + cloudId), CloudDTO.class).getData();
         Assert.assertTrue(cloudDTO.getNetworks() == null || cloudDTO.getNetworks().isEmpty());
+    }
+
+    @When("^I match the network with name \"([^\"]*)\" of the cloud \"([^\"]*)\" to the PaaS resource \"([^\"]*)\"$")
+    public void I_match_the_network_with_name_of_the_cloud_to_the_PaaS_resource(String networkName, String cloudName, String paaSResourceId) throws Throwable {
+        String cloudId = Context.getInstance().getCloudId(cloudName);
+        Context.getInstance().registerRestResponse(
+                Context.getRestClientInstance().postUrlEncoded("/rest/clouds/" + cloudId + "/networks/" + networkName + "/resource",
+                        Lists.<NameValuePair> newArrayList(new BasicNameValuePair("resourceId", paaSResourceId))));
+    }
+
+    @And("^The cloud \"([^\"]*)\" should have network mapping configuration as below:$")
+    public void The_cloud_should_have_network_mapping_configuration_as_below(String cloudName, DataTable expectedMappings) throws Throwable {
+        new CloudDefinitionsSteps().I_get_the_cloud_with_id(Context.getInstance().getCloudId(cloudName));
+        CloudDTO cloudDTO = JsonUtil.read(Context.getInstance().getRestResponse(), CloudDTO.class).getData();
+        Assert.assertNotNull(cloudDTO.getMatcherConfig());
+        Assert.assertNotNull(cloudDTO.getMatcherConfig().getNetworkMapping());
+        Set<MatchedNetwork> actualNetworks = Sets.newHashSet(cloudDTO.getMatcherConfig().getMatchedNetworks());
+        Set<MatchedNetwork> expectedNetworks = Sets.newHashSet();
+        for (List<String> rows : expectedMappings.raw()) {
+            Network network = new Network();
+            String networkName = rows.get(0);
+            network.setNetworkName(networkName);
+            String cidr = rows.get(1);
+            network.setCidr(cidr);
+            int ipVersion = Integer.parseInt(rows.get(2));
+            network.setIpVersion(ipVersion);
+            String gatewayIp = rows.get(3);
+            network.setGatewayIp(gatewayIp);
+            String pasSResourceId = rows.get(4);
+            expectedNetworks.add(new MatchedNetwork(network, pasSResourceId));
+        }
+        Assert.assertEquals(expectedNetworks, actualNetworks);
+    }
+
+    @When("^I delete the mapping for the network \"([^\"]*)\" of the cloud \"([^\"]*)\"$")
+    public void I_delete_the_mapping_for_the_network_of_the_cloud(String networkName, String cloudName) throws Throwable {
+        String cloudId = Context.getInstance().getCloudId(cloudName);
+        Context.getInstance().registerRestResponse(
+                Context.getRestClientInstance().postUrlEncoded("/rest/clouds/" + cloudId + "/networks/" + networkName + "/resource",
+                        Lists.<NameValuePair> newArrayList()));
+    }
+
+    @Then("^The cloud \"([^\"]*)\" should have empty network mapping configuration$")
+    public void The_cloud_should_have_empty_network_mapping_configuration(String cloudName) throws Throwable {
+        new CloudDefinitionsSteps().I_get_the_cloud_with_id(Context.getInstance().getCloudId(cloudName));
+        CloudDTO cloudDTO = JsonUtil.read(Context.getInstance().getRestResponse(), CloudDTO.class).getData();
+        Assert.assertTrue(cloudDTO.getMatcherConfig() == null || cloudDTO.getMatcherConfig().getMatchedNetworks() == null
+                || cloudDTO.getMatcherConfig().getMatchedNetworks().isEmpty());
     }
 }
--- alien4cloud-rest-it/src/test/resources/alien/rest/cloud/cloud_networks.feature
+++ alien4cloud-rest-it/src/test/resources/alien/rest/cloud/cloud_networks.feature
@@ -20,3 +20,21 @@ Feature: Create cloud networks
       | public | 177.68.1.0/24 | 4 | 177.68.1.1 |
     When I remove the network with name "public" from the cloud "Mount doom cloud"
     Then The cloud with name "Mount doom cloud" should not have any network as resources
+
+  Scenario: Match network
+    Given I add the network with name "private" and CIDR "192.168.1.0/24" and IP version 4 and gateway "192.168.1.1" to the cloud "Mount doom cloud"
+    And I add the network with name "public" and CIDR "192.168.2.0/24" and IP version 4 and gateway "192.168.2.1" to the cloud "Mount doom cloud"
+    When I match the network with name "private" of the cloud "Mount doom cloud" to the PaaS resource "alienPrivateNetwork"
+    Then I should receive a RestResponse with no error
+    And The cloud "Mount doom cloud" should have network mapping configuration as below:
+      | private | 192.168.1.0/24 | 4 | 192.168.1.1 | alienPrivateNetwork |
+    When I match the network with name "public" of the cloud "Mount doom cloud" to the PaaS resource "alienPublicNetwork"
+    Then The cloud "Mount doom cloud" should have network mapping configuration as below:
+      | private | 192.168.1.0/24 | 4 | 192.168.1.1 | alienPrivateNetwork |
+      | public  | 192.168.2.0/24 | 4 | 192.168.2.1 | alienPublicNetwork  |
+    When I delete the mapping for the network "public" of the cloud "Mount doom cloud"
+    Then The cloud "Mount doom cloud" should have network mapping configuration as below:
+      | private | 192.168.1.0/24 | 4 | 192.168.1.1 | alienPrivateNetwork |
+    When I delete the mapping for the network "private" of the cloud "Mount doom cloud"
+    Then The cloud "Mount doom cloud" should have empty network mapping configuration
+
