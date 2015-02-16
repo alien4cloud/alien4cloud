6d454ec84ecc170412ab687346d7c588e3fc8a31
Philippe Benoit <philippe.benoit@fastconnect.fr>
2015-02-16 10:47:23 +0100

    Fix: Remove a wrong toaster in new cloud image

--- alien4cloud-ui/yo/app/scripts/cloud-images/controllers/new_cloud_image.js
+++ alien4cloud-ui/yo/app/scripts/cloud-images/controllers/new_cloud_image.js
@@ -1,20 +1,21 @@
+/* global UTILS */
 'use strict';
 
 angular.module('alienUiApp').controller(
-  'NewCloudImageController', [
-    '$scope',
-    '$modalInstance',
-    'cloudImageServices',
-    function($scope, $modalInstance, cloudImageServices) {
-
+  'NewCloudImageController', [ '$scope', '$modalInstance', 'cloudImageServices','$q',
+    function($scope, $modalInstance, cloudImageServices, $q) {
       $scope.cloudImageFormDescriptor = cloudImageServices.getFormDescriptor();
-
       $scope.cloudImage = {};
 
       $scope.save = function(cloudImage) {
-        cloudImageServices.create({}, angular.toJson(cloudImage)).$promise.then(function(success){
-          $modalInstance.close(success.data);
-        });
+        return cloudImageServices.create({}, angular.toJson(cloudImage), function success(response) {
+          if (UTILS.isDefinedAndNotNull(response.error)) {
+            var errorsHandle = $q.defer();
+            return errorsHandle.resolve(response.error);
+          } else {
+            $modalInstance.close(response.data);
+          }
+        }).$promise;
       };
 
       $scope.cancel = function() {
