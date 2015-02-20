a60dadf833c4f2a44a96dc8ed92f7a39055da706
Cédric Mourouvin <cedric.mourouvin@fastconnect.fr>
2015-02-20 18:04:05 +0100

    ALIEN-621 wip...

--- alien4cloud-core/src/main/java/alien4cloud/model/components/ConcatPropertyValue.java
+++ alien4cloud-core/src/main/java/alien4cloud/model/components/ConcatPropertyValue.java
@@ -11,5 +11,5 @@ import alien4cloud.ui.form.annotation.FormProperties;
 @FormProperties({ "function", "parameters" })
 public class ConcatPropertyValue extends AbstractPropertyValue {
     private String function;
-    private List<String> parameters;
+    private List<Object> parameters;
 }
