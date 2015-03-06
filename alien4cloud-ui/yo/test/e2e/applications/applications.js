7294d14d62fa09ec2999edec98a236a4369a5413
Philippe Benoit <philippe.benoit@fastconnect.fr>
2015-03-06 17:49:34 +0100

    fixup! ALIEN-663: Fix errors in UI tests (WiP)

--- alien4cloud-ui/yo/test/e2e/applications/applications.js
+++ alien4cloud-ui/yo/test/e2e/applications/applications.js
@@ -286,15 +286,15 @@ var expectDeploymentPropertyValue = function(id, value, editableBoolean) {
   var container = element(by.id(id));
   expect(container.isPresent()).toBe(true);
   expect(container.isDisplayed()).toBe(true);
-  var span = container.element(by.tagName('span'));
   var editable = (editableBoolean === undefined || editableBoolean === '' || editableBoolean === null) ? false : editableBoolean; // if editableBoolean not defined, false
+  var displayValue = {};
   if (editable) {
-    expect(span.getAttribute('class')).toContain('editable');
+    displayValue = container.element(by.tagName('span'));
   } else {
-    expect(span.getAttribute('class')).not.toContain('editable');
+    displayValue = container.element(by.tagName('em'));
   }
-  expect(span.isDisplayed()).toBe(true);
-  span.getText().then(function(spanText) {
+  expect(displayValue.isPresent()).toBeTruthy();
+  displayValue.getText().then(function(spanText) {
     expect(spanText.toLowerCase()).toContain(value.toString().toLowerCase());
   });
 };
