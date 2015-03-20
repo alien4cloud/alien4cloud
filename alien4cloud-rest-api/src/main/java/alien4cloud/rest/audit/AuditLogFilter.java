7400cd33a5e91c559e9d1c2f2034894825b7e467
vuminhkh <minh.khang.vu.mkv@gmail.com>
2015-03-20 12:31:18 +0100

    ALIEN-568 bootstrap autit logging

--- alien4cloud-rest-api/src/main/java/alien4cloud/rest/audit/AuditLogFilter.java
+++ alien4cloud-rest-api/src/main/java/alien4cloud/rest/audit/AuditLogFilter.java
@@ -14,6 +14,9 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerExecutionChain;
+import org.springframework.web.servlet.HandlerMapping;
 
 import alien4cloud.audit.model.AuditTrace;
 import alien4cloud.security.AuthorizationUtil;
@@ -27,6 +30,20 @@ public class AuditLogFilter extends OncePerRequestFilter implements Ordered {
 
     @Resource
     private AuditLogRepository logRepository;
+    @Resource
+    private HandlerMapping mapping;
+
+    private HandlerMethod getHandlerMethod(HttpServletRequest request) throws Exception {
+        HandlerExecutionChain handlerChain = mapping.getHandler(request);
+        if (handlerChain == null) {
+            return null;
+        }
+        if (!(handlerChain.getHandler() instanceof HandlerMethod)) {
+            return null;
+        }
+        HandlerMethod handlerMethod = (HandlerMethod) handlerChain.getHandler();
+        return handlerMethod;
+    }
 
     @Override
     public int getOrder() {
@@ -42,14 +59,22 @@ public class AuditLogFilter extends OncePerRequestFilter implements Ordered {
      * @throws IOException
      * @throws JsonProcessingException
      */
-    private AuditTrace getAuditTrace(HttpServletRequest request, HttpServletResponse response) throws JsonProcessingException, IOException {
-        // user details
-        User user = AuthorizationUtil.getCurrentUser();
+    private AuditTrace getAuditTrace(HttpServletRequest request, HttpServletResponse response, User user) throws Exception {
         if (user == null) {
             return null;
         }
+        HandlerMethod method = getHandlerMethod(request);
+        if (method == null) {
+            return null;
+        }
+        Object controllerBean = method.getBean();
+        if (controllerBean == null) {
+            return null;
+        }
+        String auditCategory = controllerBean.getClass().getSimpleName();
         // trace user info only when he is logged
         AuditTrace auditTrace = new AuditTrace();
+        auditTrace.setCategory(auditCategory);
         auditTrace.setUserName(user.getUsername());
         auditTrace.setUserFirstName(user.getFirstName());
         auditTrace.setUserLastName(user.getLastName());
@@ -72,10 +97,20 @@ public class AuditLogFilter extends OncePerRequestFilter implements Ordered {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        User user = AuthorizationUtil.getCurrentUser();
+        String contentType = request.getHeader(HttpHeaders.CONTENT_TYPE);
+        if (user != null && contentType != null && contentType.startsWith(MediaType.APPLICATION_JSON_VALUE)) {
+            request = new MultiReadHttpServletRequest(request);
+        }
         try {
             filterChain.doFilter(request, response);
         } finally {
-            AuditTrace auditTrace = getAuditTrace(request, response);
+            AuditTrace auditTrace = null;
+            try {
+                auditTrace = getAuditTrace(request, response, user);
+            } catch (Exception e) {
+                logger.warn("Unable to get audit trace", e);
+            }
             // username exists only if it's real service (not css, js, html files)
             if (auditTrace != null) {
                 this.logRepository.add(auditTrace);
