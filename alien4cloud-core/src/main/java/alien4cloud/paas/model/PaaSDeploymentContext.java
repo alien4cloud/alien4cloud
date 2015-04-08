12a5e731b31ec73089e6b2d8006a97058aba8663
Philippe Benoit <philippe.benoit@fastconnect.fr>
2015-04-08 11:41:18 +0200

    Deployment id is now something readable so it should be used also as the name of the blueprint actually

--- alien4cloud-core/src/main/java/alien4cloud/paas/model/PaaSDeploymentContext.java
+++ alien4cloud-core/src/main/java/alien4cloud/paas/model/PaaSDeploymentContext.java
@@ -17,15 +17,4 @@ public class PaaSDeploymentContext {
      * For the same recipe, we might have multiple deployment ids.
      */
     private String deploymentId;
-
-    /**
-     * Name of an deployment (install/uninstall operation)
-     */
-    private String deploymentName;
-
-    /**
-     * A recipe's id represents in general the topology + setup in alien's term.
-     * For ex : cloudify 3 will use this information to name the blueprint and deployment.
-     */
-    private String recipeId;
 }
