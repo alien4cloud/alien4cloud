02aa4bfc9e64284fc20dac2bff21fb66a6361e23
Philippe Benoit <philippe.benoit@fastconnect.fr>
2015-05-21 14:07:42 +0200

    ALIEN-942: Clean the meta_properties.js and add comments

--- alien4cloud-ui/yo/app/scripts/controllers/meta_properties.js
+++ alien4cloud-ui/yo/app/scripts/controllers/meta_properties.js
@@ -4,19 +4,7 @@
 angular.module('alienUiApp').controller('MetaPropertiesCtrl', ['$scope', 'propertiesServices', '$translate', 'applicationServices', 'cloudServices',
   function($scope, propertiesServices, $translate, applicationServices, cloudServices) {
 
-    $scope.updateProperties = function(type, propertyDefinitionId, value) {
-      var updateApplicationPropertyObject = {
-        'definitionId': propertyDefinitionId,
-        'value': value
-      };
-      if (UTILS.isDefinedAndNotNull($scope.application)) {
-        $scope.updateApplicationMetaProperties(updateApplicationPropertyObject);
-      } else if (UTILS.isDefinedAndNotNull($scope.cloud)) {
-        $scope.updateCloudMetaProperties(updateApplicationPropertyObject);
-      }
-    };
-
-    $scope.updateApplicationMetaProperties = function(updateApplicationPropertyObject) {
+    var updateApplicationMetaProperties = function(updateApplicationPropertyObject) {
       return applicationServices.upsertProperty({
         applicationId: $scope.application.id
       }, angular.toJson(updateApplicationPropertyObject), function(response) {
@@ -26,7 +14,7 @@ angular.module('alienUiApp').controller('MetaPropertiesCtrl', ['$scope', 'proper
       });
     };
 
-    $scope.updateCloudMetaProperties = function(updateApplicationPropertyObject) {
+    var updateCloudMetaProperties = function(updateApplicationPropertyObject) {
       return cloudServices.upsertProperty({
         cloudId: $scope.cloud.id
       }, angular.toJson(updateApplicationPropertyObject), function(response) {
@@ -36,8 +24,23 @@ angular.module('alienUiApp').controller('MetaPropertiesCtrl', ['$scope', 'proper
       });
     };
 
-    $scope.getPropertyValue = function(metaPropId) {
+    /* Call the appropriate service */
+    $scope.updateProperties = function(type, propertyDefinitionId, value) {
+      var updateApplicationPropertyObject = {
+        'definitionId': propertyDefinitionId,
+        'value': value
+      };
+      // In future, we need to add component here
       if (UTILS.isDefinedAndNotNull($scope.application)) {
+        updateApplicationMetaProperties(updateApplicationPropertyObject);
+      } else if (UTILS.isDefinedAndNotNull($scope.cloud)) {
+        updateCloudMetaProperties(updateApplicationPropertyObject);
+      }
+    };
+
+    /* Return the current value */
+    $scope.getPropertyValue = function(metaPropId) {
+      if (UTILS.isDefinedAndNotNull($scope.application) && UTILS.isDefinedAndNotNull($scope.application.metaProperties)) {
         return $scope.application.metaProperties[metaPropId];
       } else if (UTILS.isDefinedAndNotNull($scope.cloud) && UTILS.isDefinedAndNotNull($scope.cloud.metaProperties)) {
         return $scope.cloud.metaProperties[metaPropId];
