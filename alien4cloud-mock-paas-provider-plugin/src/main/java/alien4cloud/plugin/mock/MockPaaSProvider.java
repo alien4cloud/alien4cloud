a4d9d85fcd5cc966aa56e95f565d78ef93324ffb
Igor Ngouagna <igor.ngouagna@fastconnect.fr>
2015-05-29 19:00:23 +0200

    fixed compilation error

--- alien4cloud-mock-paas-provider-plugin/src/main/java/alien4cloud/plugin/mock/MockPaaSProvider.java
+++ alien4cloud-mock-paas-provider-plugin/src/main/java/alien4cloud/plugin/mock/MockPaaSProvider.java
@@ -122,7 +122,8 @@ public class MockPaaSProvider extends AbstractPaaSProvider {
         attributes.put(TOSCA_NAME, "TOSCA-Simple-Profile-YAML");
         Map<String, String> runtimeProperties = Maps.newHashMap();
         runtimeProperties.put(PUBLIC_IP, "10.52.0." + i);
-        return new InstanceInformation(ToscaNodeLifecycleConstants.INITIAL, InstanceStatus.PROCESSING, attributes, runtimeProperties);
+        Map<String, String> outputs = Maps.newHashMap();
+        return new InstanceInformation(ToscaNodeLifecycleConstants.INITIAL, InstanceStatus.PROCESSING, attributes, runtimeProperties, outputs);
     }
 
     private ScalingPolicy getScalingPolicy(String id, Map<String, ScalingPolicy> policies, Map<String, NodeTemplate> nodeTemplates) {
@@ -416,7 +417,8 @@ public class MockPaaSProvider extends AbstractPaaSProvider {
     }
 
     @Override
-    public void getInstancesInformation(PaaSDeploymentContext deploymentContext, Topology topology, IPaaSCallback<Map<String, Map<String, InstanceInformation>>> callback) {
+    public void getInstancesInformation(PaaSDeploymentContext deploymentContext, Topology topology,
+            IPaaSCallback<Map<String, Map<String, InstanceInformation>>> callback) {
         MockRuntimeDeploymentInfo runtimeDeploymentInfo = runtimeDeploymentInfos.get(deploymentContext.getDeploymentPaaSId());
         if (runtimeDeploymentInfo != null) {
             callback.onSuccess(runtimeDeploymentInfo.getInstanceInformations());
