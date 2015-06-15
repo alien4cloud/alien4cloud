fd19db179b54df1110bafa430dc8e05e323578a5
Luc Boutier <luc.boutier@me.com>
2015-06-15 16:58:43 +0200

    [ALIEN-624] Moving quick search. Applications refactoring

--- alien4cloud-ui/src/main/webapp/scripts/applications/services/deployment_event_services.js
+++ alien4cloud-ui/src/main/webapp/scripts/applications/services/deployment_event_services.js
@@ -1,87 +1,95 @@
-/* global UTILS */
 
-'use strict';
 
-angular.module('alienUiApp').factory('deploymentEventServicesFactory', ['webSocketServices',
-  function(webSocketServices) {
+define(function (require) {
+  'use strict';
 
-    return function(deploymentId, listeners) {
+  var modules = require('modules');
+  var _ = require('lodash');
 
-      var allListeners;
+  require('scripts/common/services/websocket_services');
 
-      var topicFactory = function(type) {
-        return  '/topic/deployment-events/' + deploymentId + '/' + type;
-      };
+  modules.get('a4c-applications').factory('deploymentEventServicesFactory', ['webSocketServices',
+    function(webSocketServices) {
 
-      var listenerFactory = function(type, listeners) {
-        return function(event) {
-          var parsedEvent = JSON.parse(event.body);
-          for (var listenerId in listeners) {
-            if (listeners.hasOwnProperty(listenerId)) {
-              listeners[listenerId](type, parsedEvent);
+      return function(deploymentId, listeners) {
+
+        var allListeners;
+
+        var topicFactory = function(type) {
+          return  '/topic/deployment-events/' + deploymentId + '/' + type;
+        };
+
+        var listenerFactory = function(type, listeners) {
+          return function(event) {
+            var parsedEvent = JSON.parse(event.body);
+            for (var listenerId in listeners) {
+              if (listeners.hasOwnProperty(listenerId)) {
+                listeners[listenerId](type, parsedEvent);
+              }
+            }
+          };
+        };
+
+        if (_.defined(listeners)) {
+          allListeners = listeners;
+          for (var type in allListeners) {
+            if (allListeners.hasOwnProperty(type)) {
+              var topicName = topicFactory(type);
+              if (Object.keys(allListeners[type]).length > 0 && !webSocketServices.isTopicSubscribed(topicName)) {
+                webSocketServices.subscribe(topicName, listenerFactory(type, allListeners[type]));
+              }
             }
           }
-        };
-      };
+        } else {
+          allListeners = {
+            paasdeploymentstatusmonitorevent: {},
+            paasinstancestatemonitorevent: {},
+            paasmessagemonitorevent: {},
+            paasinstancestoragemonitorevent: {}
+          };
+        }
 
-      if (UTILS.isDefinedAndNotNull(listeners)) {
-        allListeners = listeners;
-        for (var type in allListeners) {
+        var close = function() {
+          for (var type in allListeners) {
+            if (allListeners.hasOwnProperty(type)) {
+              var topicName = topicFactory(type);
+              if (webSocketServices.isTopicSubscribed(topicName)) {
+                webSocketServices.unSubscribe(topicName);
+              }
+            }
+          }
+          allListeners = null;
+        };
+
+        var subscribe = function(listenerId, type, callback) {
           if (allListeners.hasOwnProperty(type)) {
+            allListeners[type][listenerId] = callback;
             var topicName = topicFactory(type);
-            if (Object.keys(allListeners[type]).length > 0 && !webSocketServices.isTopicSubscribed(topicName)) {
+            if (!webSocketServices.isTopicSubscribed(topicName)) {
               webSocketServices.subscribe(topicName, listenerFactory(type, allListeners[type]));
             }
           }
-        }
-      } else {
-        allListeners = {
-          paasdeploymentstatusmonitorevent: {},
-          paasinstancestatemonitorevent: {},
-          paasmessagemonitorevent: {},
-          paasinstancestoragemonitorevent: {}
         };
-      }
 
-      var close = function() {
-        for (var type in allListeners) {
+        var unsubscribe = function(listenerId, type) {
           if (allListeners.hasOwnProperty(type)) {
-            var topicName = topicFactory(type);
-            if (webSocketServices.isTopicSubscribed(topicName)) {
-              webSocketServices.unSubscribe(topicName);
+            delete allListeners[type][listenerId];
+            if (Object.keys(allListeners[type]).length === 0) {
+              var topicName = topicFactory(type);
+              if (webSocketServices.isTopicSubscribed(topicName)) {
+                webSocketServices.unSubscribe(topicName);
+              }
             }
           }
-        }
-        allListeners = null;
-      };
+        };
 
-      var subscribe = function(listenerId, type, callback) {
-        if (allListeners.hasOwnProperty(type)) {
-          allListeners[type][listenerId] = callback;
-          var topicName = topicFactory(type);
-          if (!webSocketServices.isTopicSubscribed(topicName)) {
-            webSocketServices.subscribe(topicName, listenerFactory(type, allListeners[type]));
-          }
-        }
+        return {
+          'allListeners': allListeners,
+          'close': close,
+          'subscribe': subscribe,
+          'unsubscribe': unsubscribe
+        };
       };
-
-      var unsubscribe = function(listenerId, type) {
-        if (allListeners.hasOwnProperty(type)) {
-          delete allListeners[type][listenerId];
-          if (Object.keys(allListeners[type]).length === 0) {
-            var topicName = topicFactory(type);
-            if (webSocketServices.isTopicSubscribed(topicName)) {
-              webSocketServices.unSubscribe(topicName);
-            }
-          }
-        }
-      };
-
-      return {
-        'allListeners': allListeners,
-        'close': close,
-        'subscribe': subscribe,
-        'unsubscribe': unsubscribe
-      };
-    };
-  }]);
+    }
+  ]); // factory
+}); // define
