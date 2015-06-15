7efdef4e69bed9550f0ab67f11b8b7a24d17f268
Luc Boutier <luc.boutier@me.com>
2015-06-15 19:19:36 +0200

    [ALIEN-624] User & group services refactoring

---
+++ alien4cloud-ui/src/main/webapp/scripts/users/directives/users.js
@@ -0,0 +1,35 @@
+define(function (require) {
+  'use strict';
+  
+  var modules = require('modules');
+  require('scripts/users/controllers/users_directive_ctrl');
+
+  modules.get('a4c-security').directive('alienSearchUser', function() {
+    return {
+      templateUrl: 'views/users/search_users_directive.html',
+      restrict: 'E',
+      scope: {
+        'crudSupport': '=',
+        'managedAppRoleList': '=',
+        'managedEnvRoleList': '=',
+        'checkAppRoleSelectedCallback': '&',
+        'checkEnvRoleSelectedCallback': '&',
+        'onSelectAppRoleCallback': '&',
+        'onSelectEnvRoleCallback': '&',
+        'onSelectAppGroupCallback': '&',
+        'onSelectEnvGroupCallback': '&',
+        'displayAll': '=',
+        'displayEmail': '=',
+        'displayRoles': '='
+      },
+      link: function postLink(scope, element, attrs) {
+        if (!attrs.checkAppRoleSelectedCallback) {
+          scope.checkAppRoleSelectedCallback = null;
+        }
+        if (!attrs.checkEnvRoleSelectedCallback) {
+          scope.checkEnvRoleSelectedCallback = null;
+        }
+      }
+    };
+  });
+});
