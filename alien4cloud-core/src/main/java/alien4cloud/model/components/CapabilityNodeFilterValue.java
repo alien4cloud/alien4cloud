37bf73047af5658ca556e6f1a1a8126028da357c
Cédric Mourouvin <mourouvin.cedric@gmail.com>
2015-06-16 15:00:15 +0200

    ALIEN-990 model v1 for node filters on requirements

--- alien4cloud-core/src/main/java/alien4cloud/model/components/CapabilityNodeFilterValue.java
+++ alien4cloud-core/src/main/java/alien4cloud/model/components/CapabilityNodeFilterValue.java
@@ -1,25 +1,30 @@
 package alien4cloud.model.components;
 
 import java.util.List;
+import java.util.Map;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ * Simple capability filter value
+ * 
+ * @author cem
+ *
+ */
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class CapabilityNodeFilterValue implements INodeFilterValue {
 
-    /** The identifier could be a property name or a type */
-    private String identifier;
-    private List<PropertyFilterDefinition> constraints;
+    /** The string identifier could be a property name or a type */
+    private Map<String, List<PropertyFilterDefinition>> capabilitiesFilter;
 
     @Override
     public boolean isCapabilityFilter() {
         return true;
     }
-
 }
--- alien4cloud-core/src/main/java/alien4cloud/model/components/PropertyNodeFilterValue.java
+++ alien4cloud-core/src/main/java/alien4cloud/model/components/PropertyNodeFilterValue.java
@@ -1,10 +1,28 @@
 package alien4cloud.model.components;
 
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Simple property filter value
+ * 
+ * @author cem
+ *
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PropertyNodeFilterValue implements INodeFilterValue {
+
+    private List<PropertyFilterDefinition> propertiesFilter;
 
     @Override
     public boolean isCapabilityFilter() {
         return false;
     }
-
 }
