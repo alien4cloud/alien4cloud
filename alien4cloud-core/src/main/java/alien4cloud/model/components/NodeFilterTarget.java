c30081b540e61be6d59128260f6e80a9e40b13a7
Cédric Mourouvin <mourouvin.cedric@gmail.com>
2015-06-16 14:07:51 +0200

    ALIEN-954 990 model bootstrap

---
+++ alien4cloud-core/src/main/java/alien4cloud/model/components/NodeFilterTarget.java
@@ -0,0 +1,11 @@
+package alien4cloud.model.components;
+
+/**
+ * Possible target for node filters
+ * 
+ * @author cem
+ *
+ */
+public enum NodeFilterTarget {
+    PROPERTIES, CAPABILITIES
+}
---
+++ alien4cloud-core/src/main/java/alien4cloud/model/components/PropertyNodeFilterValue.java
@@ -0,0 +1,10 @@
+package alien4cloud.model.components;
+
+public class PropertyNodeFilterValue implements INodeFilterValue {
+
+    @Override
+    public boolean isCapabilityFilter() {
+        return false;
+    }
+
+}
