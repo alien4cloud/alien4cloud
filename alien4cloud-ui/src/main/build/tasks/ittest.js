68cc4b2591f6c234532d8ccb14a2881eab8244b5
Luc Boutier <luc.boutier@me.com>
2015-06-16 16:56:50 +0200

    [ALIEN-624] Configure tasks so splitted ui tests run on chrome

--- alien4cloud-ui/src/main/build/tasks/ittest.js
+++ alien4cloud-ui/src/main/build/tasks/ittest.js
@@ -7,22 +7,26 @@ module.exports = function (grunt) {
     grunt.registerTask(name, itTasks);
   };
 
-  var chromeTasks = tasks.slice(0);
-  chromeTasks.push('protractor:runChrome');
-  grunt.registerTask('chrome-ittest', '', function() {
-    var tasks = chromeTasks;
-    grunt.option('force', true);
-    grunt.task.run(tasks);
-  });
+  var registerChromeIt = function(name, launchTask) {
+    var chromeTasks = tasks.slice(0);
+    chromeTasks.push(launchTask);
+    grunt.registerTask(name, '', function() {
+      var tasks = chromeTasks;
+      grunt.option('force', true);
+      grunt.task.run(tasks);
+    });
+  }
+
+  registerChromeIt('chrome-ittest', 'protractor:runChrome');
 
   registerIt('firefox-ittest', 'protractor:runFirefox');
   registerIt('iexplore-ittest', 'protractor:runIexplore');
   registerIt('local-ittest', 'protractor:runLocalserver');
-  registerIt('ittest-admin', 'protractor:runAdmin');
-  registerIt('ittest-application', 'protractor:runApplication');
-  registerIt('ittest-applicationTopology', 'protractor:runApplicationTopology');
-  registerIt('ittest-deploymentAndSecurity', 'protractor:runDeploymentAndSecurity');
-  registerIt('ittest-components', 'protractor:runComponents');
-  registerIt('ittest-common', 'protractor:runCommon');
-  registerIt('ittest-topologyTemplates', 'protractor:runTopologyTemplates');
+  registerChromeIt('ittest-admin', 'protractor:runAdmin');
+  registerChromeIt('ittest-application', 'protractor:runApplication');
+  registerChromeIt('ittest-applicationTopology', 'protractor:runApplicationTopology');
+  registerChromeIt('ittest-deploymentAndSecurity', 'protractor:runDeploymentAndSecurity');
+  registerChromeIt('ittest-components', 'protractor:runComponents');
+  registerChromeIt('ittest-common', 'protractor:runCommon');
+  registerChromeIt('ittest-topologyTemplates', 'protractor:runTopologyTemplates');
 };
