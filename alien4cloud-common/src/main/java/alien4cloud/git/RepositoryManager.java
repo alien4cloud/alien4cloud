452e6fd42e638a3b966f9917b48f6daec0bbcb09
franck-romano <franck.romano@fastconnect.fr>
2015-06-19 17:44:39 +0200

    ALIEN-989: Add repository option to trigger the import with a sub-folder or not

--- alien4cloud-common/src/main/java/alien4cloud/git/RepositoryManager.java
+++ alien4cloud-common/src/main/java/alien4cloud/git/RepositoryManager.java
@@ -5,7 +5,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
@@ -14,6 +16,7 @@ import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.springframework.beans.factory.annotation.Value;
 
+import alien4cloud.exception.AlreadyExistException;
 import alien4cloud.utils.FileUtil;
 
 /**
@@ -22,12 +25,14 @@ import alien4cloud.utils.FileUtil;
 @Slf4j
 public class RepositoryManager {
     @Getter
+    private Map<String, String> locations = new HashMap<String, String>();
+    @Getter
     private ArrayList<Path> csarsToImport = new ArrayList<Path>();
     @Getter
     private Path pathToReach;
     @Value("${directories.alien}/${directories.upload_temp}")
     private String alienTempUpload;
-    public static String _SUFFIXE = "_ZIPPED.zip";
+    public static String _SUFFIXE = "_ZIPPED";
     public static String _DEFAULTSEPARATOR = "/";
 
     public void cloneOrCheckout(Path targetDirectory, String repositoryUrl, String branch, String localDirectory) {
@@ -48,21 +53,29 @@ public class RepositoryManager {
     }
 
     /**
-     * Clone and zip
+     * Clone or checkout a GitHub Repository and its sub-branch contained in a Map<Url,Branch>
      * 
      * @param alienTpmPath The folder to clone the repositories
      * @param repositoryUrl The GitHub url to reach the repository
-     * @param branch Name of branch (i.e : master,develop etc)
+     * @param branchMap A Map of the sub-repositories with its branchId (i.e : master,develop etc)
      * @param localDirectory Static path to be resolved with targetDirectory
      */
-    public void createFolderAndCloneAndZip(Path alienTpmPath, String repositoryUrl, List<String> branchs, String localDirectory) {
+    public void createFolderAndClone(Path alienTpmPath, String repositoryUrl, Map<String, String> branchMap, String localDirectory) {
         try {
             this.pathToReach = alienTpmPath.resolve(localDirectory);
             Files.createDirectories(pathToReach);
-            for (String branch : branchs) {
-                cloneRepositoryWithCheck(repositoryUrl, branch, pathToReach.resolve(branch));
+            this.locations = branchMap;
+            String folder = this.splitRepositoryName(repositoryUrl);
+            for (Map.Entry<String, String> location : locations.entrySet()) {
+                if (location.getKey() == null || location.getKey() == "") {
+                    cloneRepositoryWithCheck(repositoryUrl, pathToReach.resolve(folder));
+                    zipRepository(this.pathToReach.resolve(folder));
+                } else {
+                    cloneRepositoryWithCheck(repositoryUrl, pathToReach.resolve(folder));
+                    zipRepository(this.pathToReach.resolve(folder).resolve(location.getKey()), this.getLocations());
+                }
             }
-            zipRepository(this.pathToReach, branchs);
+
         } catch (IOException e) {
             log.error("Error while creating target directory ", e);
         }
@@ -85,6 +98,7 @@ public class RepositoryManager {
         }
     }
 
+
     /**
      * Check and clone a repository from its url if the repository has already been checked-out
      * 
@@ -92,12 +106,12 @@ public class RepositoryManager {
      * @param branch Specified branch to clone
      * @param targetPath Path of the folder to checkout the repository
      */
-    private void cloneRepositoryWithCheck(String url, String branch, Path targetPath) {
+    private void cloneRepositoryWithCheck(String url, Path targetPath) {
         Git result;
         if (!Files.exists(targetPath)) {
-            log.info("Cloning from [" + url + "] branch [" + branch + "] to [" + targetPath.toString() + "]");
+            log.info("Cloning from [" + url + "] to [" + targetPath.toString() + "]");
             try {
-                result = Git.cloneRepository().setURI(url).setBranch(branch).setDirectory(targetPath.toFile()).call();
+                result = Git.cloneRepository().setURI(url).setDirectory(targetPath.toFile()).call();
                 try {
                     log.info("Cloned: " + result.getRepository().getDirectory());
                 } finally {
@@ -108,24 +122,23 @@ public class RepositoryManager {
             }
         } else {
             log.error("Csar " + targetPath + " have been already cloned from Github");
-         //   throw new AlreadyExistException("Csar have been already cloned from Github");
+            throw new AlreadyExistException("Csar have been already cloned from Github");
         }
     }
 
     /**
-     * Zip a folder and its CSARs
+     * Zip a folder and its CSARs with any importLocations
      * 
-     * If the repository is already and archive the folder is zippe directly
+     * If the repository is already and archive the folder is zipped directly
      * Else, only the sub-repos containing the Yaml file are zipped.
      * 
      * @param pathToFetch The path where the parent folder is stored
      * @param locations The sub-folders to zip
      */
-    private void zipRepository(Path pathToFetch, List<String> branchs) {
+    private void zipRepository(Path pathToFetch, Map<String, String> locations) {
         File file = pathToFetch.toFile();
-        for (String branch : branchs) {
+        for (Entry<String, String> entry : locations.entrySet()) {
             try {
-                file = pathToFetch.resolve(branch).toFile();
                 File[] listFiles = file.listFiles();
                 if (!isArchive(listFiles)) {
                     for (int i = 0; i < listFiles.length; i++) {
@@ -135,12 +148,40 @@ public class RepositoryManager {
                         }
                     }
                 } else {
-                    FileUtil.zip(pathToFetch.resolve(branch), pathToFetch.resolve(branch + _SUFFIXE));
-                    this.csarsToImport.add(pathToFetch.resolve(branch + _SUFFIXE));
+                    FileUtil.zip(pathToFetch, pathToFetch.resolve(entry.getKey() + _SUFFIXE));
+                    this.csarsToImport.add(pathToFetch.resolve(entry.getKey() + _SUFFIXE));
                 }
             } catch (IOException e) {
                 log.error("Error while zipping target directory ", e);
             }
+        }
+    }
+
+    /**
+     * Zip a folder and its CSARs without any importLocations
+     * 
+     * If the repository is already and archive the folder is zipped directly
+     * Else, only the sub-repos containing the Yaml file are zipped.
+     * 
+     * @param pathToFetch The path where the parent folder is stored 
+     */
+    private void zipRepository(Path pathToFetch) {
+        File file = pathToFetch.toFile();
+        try {
+            File[] listFiles = file.listFiles();
+            if (!isArchive(listFiles)) {
+                for (int i = 0; i < listFiles.length; i++) {
+                    if (listFiles[i].isDirectory() && !listFiles[i].getName().endsWith(".git")) {
+                        FileUtil.zip(file.toPath().resolve(listFiles[i].getName()), pathToFetch.resolve(listFiles[i].getName() + _SUFFIXE));
+                        this.csarsToImport.add(pathToFetch.resolve(listFiles[i].getName() + _SUFFIXE));
+                    }
+                }
+            } else {
+                FileUtil.zip(pathToFetch, pathToFetch.resolve(splitRepositoryName(file.getName() + _SUFFIXE)));
+                this.csarsToImport.add(pathToFetch.resolve(file.getName() + _SUFFIXE));
+            }
+        } catch (IOException e) {
+            log.error("Error while zipping target directory ", e);
         }
     }
 
@@ -159,5 +200,15 @@ public class RepositoryManager {
             }
         }
         return cpt > 2 ? false : true;
+    }
+
+    /**
+     * Retrieve the repository name of an url based on the last "/".
+     * @param url The url to parse
+     * @return The repository name
+     */
+    private String splitRepositoryName(String url) {
+        String[] urlSplit = url.split("/");
+        return url.split("/")[urlSplit.length-1];
     }
 }
--- alien4cloud-core/src/main/java/alien4cloud/csar/services/CsarGithubService.java
+++ alien4cloud-core/src/main/java/alien4cloud/csar/services/CsarGithubService.java
@@ -3,9 +3,11 @@ package alien4cloud.csar.services;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -49,7 +51,7 @@ public class CsarGithubService {
      * @param username The username of the user
      * @param password The password of the user
      * @param importLocations Locations where Csar's files are store
-     * @return
+     * @return The auto-generated id of the CsarGitRepository object
      */
     public String createGithubCsar(String repositoryUrl, String username, String password, List<CsarGitCheckoutLocation> importLocations) {
         CsarGitRepository csarGit = new CsarGitRepository();
@@ -65,33 +67,28 @@ public class CsarGithubService {
      * Method to trigger the checkout of a CsarGitRepository from Github
      * 
      * @param param The unique id of the CsarGitRepository to trigger
-     * @return
+     * @return An response if the statement was successful or not
      * @throws ParsingException
      * @throws CSARVersionAlreadyExistsException
      */
     public ParsingResult<Csar>[] specifyCsarFromGit(String param) throws CSARVersionAlreadyExistsException, ParsingException {
         CsarGitRepository csarGit = new CsarGitRepository();
+        Map<String, String> locationsMap = new HashMap<String, String>();
         String data = param.replaceAll("\"", "");
         if (!paramIsUrl(data)) {
             csarGit = alienCsarGitDao.findById(CsarGitRepository.class, data);
         } else {
             csarGit = getCsargitByUrl(data);
         }
-        List<String> branchs = new LinkedList<String>();
-        List<CsarGitCheckoutLocation> locationsList;
         RepositoryManager repoManager = new RepositoryManager();
         Path alienTpmPath = Paths.get(alienTempUpload);
         if (csarGit == null) {
             throw new NotFoundException("CsarGit " + "[" + data + "] doesn't exist");
         }
-        locationsList = csarGit.getImportLocations();
-        if (locationsList.isEmpty()) {
-            throw new NotFoundException("CsarGit " + "[" + data + "] doesn't contain any import locations");
-        }
         for (CsarGitCheckoutLocation location : csarGit.getImportLocations()) {
-            branchs.add(location.getBranchId());
+            locationsMap.put(location.getSubPath(), location.getBranchId());
         }
-        repoManager.createFolderAndCloneAndZip(alienTpmPath, csarGit.getRepositoryUrl(), branchs, _LOCALDIRECTORY);
+        repoManager.createFolderAndClone(alienTpmPath, csarGit.getRepositoryUrl(), locationsMap, _LOCALDIRECTORY);
         return triggerImportFromTmpFolder(repoManager.getPathToReach(), repoManager.getCsarsToImport());
     }
 
@@ -99,7 +96,7 @@ public class CsarGithubService {
      * Handle the import of CsarGitRepository (importLocations) in Alien
      * 
      * @param folderPath The path to fetch where Csars have been checked out.
-     * @return
+     * @return An response if the statement was successful or not
      * @throws ParsingException
      * @throws CSARVersionAlreadyExistsException
      */
@@ -134,6 +131,12 @@ public class CsarGithubService {
         }
     };
 
+    /**
+     * Add a location into an existing CsarGitRepository object
+     * 
+     * @param id The unique id of the CsarGitRepository
+     * @param locations Locations to add
+     */
     public void addImportLocation(String id, List<CsarGitCheckoutLocation> locations) {
         if (locations.isEmpty() || locations == null) {
             throw new NotFoundException("Checkout location is empty");
@@ -145,6 +148,12 @@ public class CsarGithubService {
         }
     };
 
+    /**
+     * Remove a location of an existing CsarGitRepository object
+     * 
+     * @param id The unique id of the CsarGitRepository
+     * @param branchId The unique branch id
+     */
     public void removeImportLocation(String id, String branchId) {
         CsarGitRepository csarGit = checkIfCsarExist(id);
         if (csarGit == null) {
