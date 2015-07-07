78867faa0cb743e1582c1444dc1feaeba9da57d8
Luc Boutier <luc.boutier@me.com>
2015-07-07 11:34:40 +0200

    - Add warning for unknown properties when valueIsNode is false.

--- alien4cloud-core/src/main/java/alien4cloud/tosca/parser/impl/base/SequenceToMapParser.java
+++ alien4cloud-core/src/main/java/alien4cloud/tosca/parser/impl/base/SequenceToMapParser.java
@@ -2,17 +2,13 @@ package alien4cloud.tosca.parser.impl.base;
 
 import java.util.Map;
 
+import alien4cloud.tosca.parser.*;
+import alien4cloud.tosca.parser.impl.ErrorCode;
 import lombok.AllArgsConstructor;
 
 import org.elasticsearch.common.collect.Maps;
-import org.yaml.snakeyaml.nodes.MappingNode;
-import org.yaml.snakeyaml.nodes.Node;
-import org.yaml.snakeyaml.nodes.ScalarNode;
-import org.yaml.snakeyaml.nodes.SequenceNode;
-
-import alien4cloud.tosca.parser.INodeParser;
-import alien4cloud.tosca.parser.ParserUtils;
-import alien4cloud.tosca.parser.ParsingContextExecution;
+import org.yaml.snakeyaml.error.Mark;
+import org.yaml.snakeyaml.nodes.*;
 
 /**
  * Parse a yaml sequence into a {@link Map}
@@ -24,7 +20,7 @@ public class SequenceToMapParser<T> implements INodeParser<Map<String, T>> {
     private INodeParser<T> valueParser;
     /** The tosca type of the map. */
     private String toscaType;
-    /** If the sequence element mapping node is also the  */
+    /** If the sequence element mapping node is also the */
     private Boolean nodeIsValue;
 
     @Override
@@ -41,6 +37,7 @@ public class SequenceToMapParser<T> implements INodeParser<Map<String, T>> {
                         value = valueParser.parse(mappingNode, context);
                     } else {
                         value = valueParser.parse(mappingNode.getValue().get(0).getValueNode(), context);
+                        checkMappingNodeSingleProperty(mappingNode, context);
                     }
                     sequenceMap.put(key, value);
                 } else {
@@ -53,6 +50,25 @@ public class SequenceToMapParser<T> implements INodeParser<Map<String, T>> {
 
         ParserUtils.addTypeError(node, context.getParsingErrors(), toscaType);
         return null;
+    }
+
+    /**
+     * Check that the mapping node has a single property (when value is node is false other properties are ignored).
+     * 
+     * @param mappingNode The mapping node.
+     * @param context The parsing context.
+     */
+    public void checkMappingNodeSingleProperty(MappingNode mappingNode, ParsingContextExecution context) {
+        if (mappingNode.getValue().size() > 1) {
+            // some properties are ignored.
+            for (int i = 1; i < mappingNode.getValue().size(); i++) {
+                NodeTuple entry = mappingNode.getValue().get(i);
+                ParsingError err = new ParsingError(ParsingErrorLevel.WARNING, ErrorCode.UNRECOGNIZED_PROPERTY, "Parsing a MappingNode as a Map", entry
+                        .getValueNode().getStartMark(), "The value of this tuple should be a scalar", entry.getValueNode().getEndMark(),
+                        ((ScalarNode) entry.getKeyNode()).getValue());
+                context.getParsingErrors().add(err);
+            }
+        }
     }
 
     @Override
