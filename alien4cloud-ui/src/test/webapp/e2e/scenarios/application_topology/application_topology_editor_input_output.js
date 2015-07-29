3125d2c48ac4049b0d276b618cb70c5b887d31ac
Philippe Benoit <philippe.benoit@fastconnect.fr>
2015-07-29 19:37:51 +0200

    ALIEN-1061: add UI test

--- alien4cloud-ui/src/test/webapp/e2e/scenarios/application_topology/application_topology_editor_input_output.js
+++ alien4cloud-ui/src/test/webapp/e2e/scenarios/application_topology/application_topology_editor_input_output.js
@@ -6,9 +6,11 @@ var common = require('../../common/common');
 var authentication = require('../../authentication/authentication');
 var navigation = require('../../common/navigation');
 var topologyEditorCommon = require('../../topology/topology_editor_common');
-var cloudsCommon = require('../../admin/clouds_common');
 var rolesCommon = require('../../common/roles_common');
 var componentData = require('../../topology/component_data');
+var tagConfigCommon = require('../../admin/metaprops_configuration_common');
+var cloudsCommon = require('../../admin/clouds_common');
+var applications = require('../../applications/applications');
 
 describe('Topology input/output properties', function() {
 
@@ -28,6 +30,45 @@ describe('Topology input/output properties', function() {
     topologyEditorCommon.expectPropertyInputState('Compute', 'disk_size', false);
     topologyEditorCommon.expectAttributeOutputState('Compute', 'ip_address', true);
   };
+
+  /* test to ensure an non-regresion on ALIEN-1061 */
+  it('should be able to define properties as input of cloud meta and update the view when we change the cloud', function() {
+    console.log('################# should be able to define properties as input of cloud meta and update the view when we change the cloud');
+    cloudsCommon.giveRightsOnCloudToUser('testcloud', 'applicationManager', rolesCommon.cloudRoles.cloudDeployer);
+    navigation.go('main', 'applications');
+    browser.element(by.binding('application.name')).click();
+    navigation.go('applications', 'topology');
+    topologyEditorCommon.addNodeTemplatesCenterAndZoom({
+      compute: componentData.toscaBaseTypes.compute()
+    });
+    topologyEditorCommon.editNodeProperty('Compute', 'os_type', 'windows');
+    topologyEditorCommon.editNodeProperty('Compute', 'containee_types', 'test', 'cap');
+    topologyEditorCommon.togglePropertyInput('Compute', 'os_distribution');
+    topologyEditorCommon.renameInput('os_distribution', 'cloud_meta_distribution');
+
+    // create 2 clouds and add a meta_cloud property
+    cloudsCommon.goToCloudList();
+    cloudsCommon.createNewCloud('testcloud-meta-empty');
+    cloudsCommon.goToCloudDetail('testcloud-meta-empty');
+    cloudsCommon.enableCloud();
+    tagConfigCommon.addTagConfiguration(tagConfigCommon.defaultCloudProperty, null);
+
+    // set a value in the cloud_meta_distribution of testcloud
+    cloudsCommon.goToCloudDetail('testcloud');
+    cloudsCommon.goToCloudConfiguration();
+    cloudsCommon.showMetaProperties();
+    expect(element(by.id('cloudMetaPropertiesDisplay')).isDisplayed()).toBe(true);
+    tagConfigCommon.editTagConfiguration('distribution', 'debian');
+
+    // now we go in deploymet page and change the cloud to test the input cloud meta validation
+    navigation.go('main', 'applications');
+    browser.element(by.binding('application.name')).click();
+    navigation.go('applications', 'deployment');
+    applications.switchEnvironmentAndCloud(null, 'testcloud');
+    topologyEditorCommon.checkWarningList(false);
+    applications.switchEnvironmentAndCloud(null, 'testcloud-meta-empty');
+    topologyEditorCommon.checkWarningList(true);
+  });
 
   it('should be able to define properties as input or output and see their values in application details view', function() {
     console.log('################# should be able to define properties as input or output and see their values in application details view');
@@ -87,6 +128,5 @@ describe('Topology input/output properties', function() {
     expect(outputTableText).not.toContain('1024');
     expect(outputTableText).not.toContain('ip_address');
     expect(outputTableText).not.toContain('10.52.0.');
-
   });
 });
