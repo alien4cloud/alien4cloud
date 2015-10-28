f4678f378e26096e0c4a5648fa46be076c9f7d73
Igor Ngouagna <igor.ngouagna@fastconnect.fr>
2015-10-28 16:59:23 +0100

    place holder directive for empty lists in ui

---
+++ alien4cloud-ui/src/main/webapp/scripts/common/directives/empty_place_holder.js
@@ -0,0 +1,21 @@
+define(function (require) {
+  'use strict';
+
+  var modules = require('modules');
+  var _ = require('lodash');
+
+  modules.get('a4c-common').directive('emptyPlaceHolder', function() {
+    return {
+      templateUrl: 'views/common/empty_place_holder.html',
+      restrict: 'E',
+      scope: {
+        'for': '=',
+        'data': '=',
+        'additionalMessage': '=',
+      },
+      link: function(scope) {
+        scope._ = _;
+      }
+    };
+  });
+}); // define
