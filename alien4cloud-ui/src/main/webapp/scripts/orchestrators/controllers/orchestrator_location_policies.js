c7e3a8d84da1bae70e82aa01760297e39de8b233
Luc Boutier <luc@fastconnect.fr>
2015-11-15 09:54:36 +0100

    Disable the policy and services tabs that will be implemented in 1.2.0

--- alien4cloud-ui/src/main/webapp/scripts/orchestrators/controllers/orchestrator_location_policies.js
+++ alien4cloud-ui/src/main/webapp/scripts/orchestrators/controllers/orchestrator_location_policies.js
@@ -6,18 +6,18 @@ define(function (require) {
   var angular = require('angular');
   var _ = require('lodash');
 
-  states.state('admin.orchestrators.details.locations.policies', {
-    url: '/policy',
-    templateUrl: 'views/orchestrators/orchestrator_location_policies.html',
-    controller: 'OrchestratorLocationsPoliciesCtrl',
-    menu: {
-      id: 'menu.orchestrators.locations.policies',
-      state: 'admin.orchestrators.details.locations.policies',
-      key: 'ORCHESTRATORS.LOCATIONS.POLICIES',
-      icon: 'fa fa-cogs',
-      priority: 400
-    }
-  });
+  // states.state('admin.orchestrators.details.locations.policies', {
+  //   url: '/policy',
+  //   templateUrl: 'views/orchestrators/orchestrator_location_policies.html',
+  //   controller: 'OrchestratorLocationsPoliciesCtrl',
+  //   menu: {
+  //     id: 'menu.orchestrators.locations.policies',
+  //     state: 'admin.orchestrators.details.locations.policies',
+  //     key: 'ORCHESTRATORS.LOCATIONS.POLICIES',
+  //     icon: 'fa fa-cogs',
+  //     priority: 400
+  //   }
+  // });
 
   modules.get('a4c-orchestrators', ['ui.router', 'ui.bootstrap','a4c-common']).controller('OrchestratorLocationsPoliciesCtrl',
     ['$scope', 'orchestrator',
