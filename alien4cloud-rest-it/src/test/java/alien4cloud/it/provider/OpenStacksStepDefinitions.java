902572693e10f6edb6303ac66eb6ea24fdc75bda
Xavier DEGENNE <xavier.degenne@fastconnect.fr>
2015-12-08 08:40:49 +0000

    add scale tests

--- alien4cloud-rest-it/src/test/java/alien4cloud/it/provider/OpenStacksStepDefinitions.java
+++ alien4cloud-rest-it/src/test/java/alien4cloud/it/provider/OpenStacksStepDefinitions.java
@@ -2,15 +2,14 @@ package alien4cloud.it.provider;
 
 import java.io.IOException;
 
-import alien4cloud.rest.deployment.DeploymentTopologyDTO;
 import org.jclouds.openstack.cinder.v1.domain.Volume;
 import org.junit.Assert;
 
 import alien4cloud.it.Context;
 import alien4cloud.it.utils.JsonTestUtil;
 import alien4cloud.paas.function.FunctionEvaluator;
+import alien4cloud.rest.deployment.DeploymentTopologyDTO;
 import alien4cloud.rest.model.RestResponse;
-import alien4cloud.topology.TopologyDTO;
 import cucumber.api.java.en.And;
 
 public class OpenStacksStepDefinitions {
@@ -33,6 +32,16 @@ public class OpenStacksStepDefinitions {
         Assert.assertNotNull(volume);
     }
 
+    @And("^I should have volumes on OpenStack with ids defined in property \"([^\"]*)\" of the node \"([^\"]*)\" for \"([^\"]*)\"$")
+    public void I_should_have_volumes_on_OpenStack_with_ids_defined_in_property_of_the_node(String propertyName, String nodeName, String appName) throws Throwable {
+        String ids = getVolumeId(propertyName, nodeName, appName);
+        String[] idsArr = ids.split(",");
+        for (String id : idsArr) {
+            Volume volume = Context.getInstance().getOpenStackClient().getVolume(id);
+            Assert.assertNotNull(volume);
+        }
+    }
+    
     @And("^I delete the volume on OpenStack with id defined in property \"([^\"]*)\" of the node \"([^\"]*)\" for \"([^\"]*)\"$")
     public void I_delete_the_volume_on_OpenStack_with_id_defined_in_property_of_the_node(String propertyName, String nodeName, String appName) throws Throwable {
         Assert.assertTrue(Context.getInstance().getOpenStackClient().deleteVolume(getVolumeId(propertyName, nodeName, appName)));
