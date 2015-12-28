a64684b53b98c92af0157bbbf894376e858c654f
Igor Ngouagna <igor.ngouagna@fastconnect.fr>
2015-12-28 10:54:15 +0100

    relocate data exporter for ui tests

--- alien4cloud-ui/src/test/java/utils/ExportESIndex.java
+++ alien4cloud-ui/src/test/java/utils/ExportESIndex.java
@@ -1,11 +1,11 @@
 package utils;
 
-import alien4cloud.utils.FileUtil;
-
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+
+import alien4cloud.utils.FileUtil;
 
 /**
  * Use this class to export all ES index as JSON for the UI tests.
@@ -13,9 +13,11 @@ import java.nio.file.Paths;
 public class ExportESIndex {
 
     public static void main(String[] args) throws IOException {
-        String pathToAlien = "/home/benoitph/.alien";
-        String pathToData = "/home/benoitph/Projets/alien4cloud/alien4cloud-ui/src/test/webapp/e2e/_data";
-
+        String pathToAlien = System.getProperty("user.home")+"/.alien";
+        String pathToData = "src/test/webapp/e2e/_data";
+        
+        //Delete images first
+        FileUtil.delete(Paths.get(pathToData + "/images"));
         FileUtil.copy(Paths.get(pathToAlien + "/images"),
                 Paths.get(pathToData + "/images"));
 
