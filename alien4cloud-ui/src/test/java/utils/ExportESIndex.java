1a96305a8ac1c940c103d42de20b0ad4a8f60250
VU Minh Khang <minh.khang.vu.mkv@gmail.com>
2015-12-30 15:23:39 +0100

    Sort export so that data respects order for comparison purpose

--- alien4cloud-ui/src/test/java/utils/ExportESIndex.java
+++ alien4cloud-ui/src/test/java/utils/ExportESIndex.java
@@ -13,55 +13,54 @@ import alien4cloud.utils.FileUtil;
 public class ExportESIndex {
 
     public static void main(String[] args) throws IOException {
-        String pathToAlien = System.getProperty("user.home")+"/.alien";
+        String pathToAlien = System.getProperty("user.home") + "/.alien";
         String pathToData = "src/test/webapp/e2e/_data";
-        
-        //Delete images first
-        FileUtil.delete(Paths.get(pathToData + "/images"));
-        FileUtil.copy(Paths.get(pathToAlien + "/images"),
-                Paths.get(pathToData + "/images"));
 
-        export("curl -X POST \"http://localhost:9200/imagedata/_search?pretty=true\" -d '{\"from\" : 0, \"size\" : 1000,\"query\" : {\"match_all\" : {}}}' | grep _source",
+        // Delete images first
+        FileUtil.delete(Paths.get(pathToData + "/images"));
+        FileUtil.copy(Paths.get(pathToAlien + "/images"), Paths.get(pathToData + "/images"));
+
+        export("curl -X POST \"http://localhost:9200/imagedata/_search?pretty=true\" -d '{\"from\" : 0, \"size\" : 1000,\"sort\": [{\"id\": {\"order\": \"asc\"}}],\"query\" : {\"match_all\" : {}}}' | grep _source",
                 Paths.get(pathToData + "/imagedatas.json"));
 
         // Application
-        export("curl -X POST \"http://localhost:9200/application/_search?pretty=true\" -d '{\"from\" : 0, \"size\" : 1000,\"query\" : {\"match_all\" : {}}}' | grep _source",
+        export("curl -X POST \"http://localhost:9200/application/_search?pretty=true\" -d '{\"from\" : 0, \"size\" : 1000,\"sort\": [{\"id\": {\"order\": \"asc\"}}],\"query\" : {\"match_all\" : {}}}' | grep _source",
                 Paths.get(pathToData + "/applications.json"));
-        export("curl -X POST \"http://localhost:9200/applicationenvironment/_search?pretty=true\" -d '{\"from\" : 0, \"size\" : 1000,\"query\" : {\"match_all\" : {}}}' | grep _source",
+        export("curl -X POST \"http://localhost:9200/applicationenvironment/_search?pretty=true\" -d '{\"from\" : 0, \"size\" : 1000,\"sort\": [{\"id\": {\"order\": \"asc\"}}],\"query\" : {\"match_all\" : {}}}' | grep _source",
                 Paths.get(pathToData + "/applicationenvironments.json"));
-        export("curl -X POST \"http://localhost:9200/applicationversion/_search?pretty=true\" -d '{\"from\" : 0, \"size\" : 1000,\"query\" : {\"match_all\" : {}}}' | grep _source",
+        export("curl -X POST \"http://localhost:9200/applicationversion/_search?pretty=true\" -d '{\"from\" : 0, \"size\" : 1000,\"sort\": [{\"id\": {\"order\": \"asc\"}}],\"query\" : {\"match_all\" : {}}}' | grep _source",
                 Paths.get(pathToData + "/applicationversions.json"));
 
         // Components
-        export("curl -X POST \"http://localhost:9200/csar/_search?pretty=true\" -d '{\"from\" : 0, \"size\" : 1000,\"query\" : {\"match_all\" : {}}}' | grep _source",
+        export("curl -X POST \"http://localhost:9200/csar/_search?pretty=true\" -d '{\"from\" : 0, \"size\" : 1000,\"sort\": [{\"id\": {\"order\": \"asc\"}}],\"query\" : {\"match_all\" : {}}}' | grep _source",
                 Paths.get(pathToData + "/csars.json"));
-        export("curl -X POST \"http://localhost:9200/csargitrepository/_search?pretty=true\" -d '{\"from\" : 0, \"size\" : 1000,\"query\" : {\"match_all\" : {}}}' | grep _source",
+        export("curl -X POST \"http://localhost:9200/csargitrepository/_search?pretty=true\" -d '{\"from\" : 0, \"size\" : 1000,\"sort\": [{\"id\": {\"order\": \"asc\"}}],\"query\" : {\"match_all\" : {}}}' | grep _source",
                 Paths.get(pathToData + "/csargitrepositories.json"));
-        export("curl -X POST \"http://localhost:9200/toscaelement/indexedartifacttype/_search?pretty=true\" -d '{\"from\" : 0, \"size\" : 1000,\"query\" : {\"match_all\" : {}}}' | grep _source",
+        export("curl -X POST \"http://localhost:9200/toscaelement/indexedartifacttype/_search?pretty=true\" -d '{\"from\" : 0, \"size\" : 1000,\"sort\": [{\"id\": {\"order\": \"asc\"}}],\"query\" : {\"match_all\" : {}}}' | grep _source",
                 Paths.get(pathToData + "/indexedartifacttypes.json"));
-        export("curl -X POST \"http://localhost:9200/toscaelement/indexedcapabilitytype/_search?pretty=true\" -d '{\"from\" : 0, \"size\" : 1000,\"query\" : {\"match_all\" : {}}}' | grep _source",
+        export("curl -X POST \"http://localhost:9200/toscaelement/indexedcapabilitytype/_search?pretty=true\" -d '{\"from\" : 0, \"size\" : 1000,\"sort\": [{\"id\": {\"order\": \"asc\"}}],\"query\" : {\"match_all\" : {}}}' | grep _source",
                 Paths.get(pathToData + "/indexedcapabilitytypes.json"));
-        export("curl -X POST \"http://localhost:9200/toscaelement/indexeddatatype/_search?pretty=true\" -d '{\"from\" : 0, \"size\" : 1000,\"query\" : {\"match_all\" : {}}}' | grep _source",
+        export("curl -X POST \"http://localhost:9200/toscaelement/indexeddatatype/_search?pretty=true\" -d '{\"from\" : 0, \"size\" : 1000,\"sort\": [{\"id\": {\"order\": \"asc\"}}],\"query\" : {\"match_all\" : {}}}' | grep _source",
                 Paths.get(pathToData + "/indexeddatatypes.json"));
-        export("curl -X POST \"http://localhost:9200/toscaelement/indexednodetype/_search?pretty=true\" -d '{\"from\" : 0, \"size\" : 1000,\"query\" : {\"match_all\" : {}}}' | grep _source",
+        export("curl -X POST \"http://localhost:9200/toscaelement/indexednodetype/_search?pretty=true\" -d '{\"from\" : 0, \"size\" : 1000,\"sort\": [{\"id\": {\"order\": \"asc\"}}],\"query\" : {\"match_all\" : {}}}' | grep _source",
                 Paths.get(pathToData + "/indexednodetypes.json"));
-        export("curl -X POST \"http://localhost:9200/toscaelement/indexedrelationshiptype/_search?pretty=true\" -d '{\"from\" : 0, \"size\" : 1000,\"query\" : {\"match_all\" : {}}}' | grep _source",
+        export("curl -X POST \"http://localhost:9200/toscaelement/indexedrelationshiptype/_search?pretty=true\" -d '{\"from\" : 0, \"size\" : 1000,\"sort\": [{\"id\": {\"order\": \"asc\"}}],\"query\" : {\"match_all\" : {}}}' | grep _source",
                 Paths.get(pathToData + "/indexedrelationshiptypes.json"));
 
         // Topology templates
-        export("curl -X POST \"http://localhost:9200/topologytemplate/_search?pretty=true\" -d '{\"from\" : 0, \"size\" : 100,\"query\" : {\"match_all\" : {}}}' | grep _source",
+        export("curl -X POST \"http://localhost:9200/topologytemplate/_search?pretty=true\" -d '{\"from\" : 0, \"size\" : 100,\"sort\": [{\"id\": {\"order\": \"asc\"}}],\"query\" : {\"match_all\" : {}}}' | grep _source",
                 Paths.get(pathToData + "/topologytemplates.json"));
-        export("curl -X POST \"http://localhost:9200/topologytemplateversion/_search?pretty=true\" -d '{\"from\" : 0, \"size\" : 100,\"query\" : {\"match_all\" : {}}}' | grep _source",
+        export("curl -X POST \"http://localhost:9200/topologytemplateversion/_search?pretty=true\" -d '{\"from\" : 0, \"size\" : 100,\"sort\": [{\"id\": {\"order\": \"asc\"}}],\"query\" : {\"match_all\" : {}}}' | grep _source",
                 Paths.get(pathToData + "/topologytemplateversions.json"));
 
         // Topologies
-        export("curl -X POST \"http://localhost:9200/topology/_search?pretty=true\" -d '{\"from\" : 0, \"size\" : 100,\"query\" : {\"match_all\" : {}}}' | grep _source",
+        export("curl -X POST \"http://localhost:9200/topology/_search?pretty=true\" -d '{\"from\" : 0, \"size\" : 100,\"sort\": [{\"id\": {\"order\": \"asc\"}}],\"query\" : {\"match_all\" : {}}}' | grep _source",
                 Paths.get(pathToData + "/topologies.json"));
 
         // Users and groups
-        export("curl -X POST \"http://localhost:9200/user/_search?pretty=true\" -d '{\"from\" : 0, \"size\" : 100,\"query\" : {\"match_all\" : {}}}' | grep _source",
+        export("curl -X POST \"http://localhost:9200/user/_search?pretty=true\" -d '{\"from\" : 0, \"size\" : 100,\"sort\": [{\"username\": {\"order\": \"asc\"}}],\"query\" : {\"match_all\" : {}}}' | grep _source",
                 Paths.get(pathToData + "/users.json"));
-        export("curl -X POST \"http://localhost:9200/group/_search?pretty=true\" -d '{\"from\" : 0, \"size\" : 100,\"query\" : {\"match_all\" : {}}}' | grep _source",
+        export("curl -X POST \"http://localhost:9200/group/_search?pretty=true\" -d '{\"from\" : 0, \"size\" : 100,\"sort\": [{\"id\": {\"order\": \"asc\"}}],\"query\" : {\"match_all\" : {}}}' | grep _source",
                 Paths.get(pathToData + "/groups.json"));
     }
 
@@ -75,7 +74,13 @@ public class ExportESIndex {
             line = line.replaceAll("\"_source\":", "");
             line = "  " + line;
             if (i < execReturn.getResultLines().length - 1) {
-                line = line + ",";
+                if (!line.endsWith(",")) {
+                    line = line + ",";
+                }
+            } else {
+                if (line.endsWith(",")) {
+                    line = line.substring(0, line.length() - 1);
+                }
             }
             line = line + System.lineSeparator();
             fw.write(line);
