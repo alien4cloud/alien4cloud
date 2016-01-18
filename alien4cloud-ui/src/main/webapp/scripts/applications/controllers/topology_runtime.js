4d73251d1b8d4fd959532936fb37f6cb4b13a4d1
Luc Boutier <luc@fastconnect.fr>
2016-01-18 14:51:21 +0100

    Fix url routing for topology template version and substitution node.

--- alien4cloud-ui/src/main/webapp/scripts/applications/controllers/topology_runtime.js
+++ alien4cloud-ui/src/main/webapp/scripts/applications/controllers/topology_runtime.js
@@ -61,7 +61,7 @@ define(function (require) {
             appEnvironments.selectedEnvironment = $scope.selectedEnvironment;
           }
         }
-      }
+      };
 
       //update the selectedEnvironment
       updateSelectedEnvionment();
