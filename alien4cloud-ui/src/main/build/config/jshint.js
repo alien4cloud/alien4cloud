7676f8b4ead5def37e8f5014fd484f5327d1c6fe
Luc Boutier <luc@fastconnect.fr>
2016-02-23 12:03:04 +0100

    Fix jshint configuration

--- alien4cloud-ui/src/main/build/config/jshint.js
+++ alien4cloud-ui/src/main/build/config/jshint.js
@@ -3,14 +3,14 @@
 // Make sure code styles are up to par and there are no obvious mistakes
 module.exports = {
   options: {
-    jshintrc: '<%= yeoman.setup %>/.jshintrc',
+    jshintrc: 'src/.jshintrc',
     reporter: require('jshint-stylish'),
     force: true
   },
-  all: ['Gruntfile.js', '<%= yeoman.app %>/scripts/{,*/}*.js'],
+  all: ['Gruntfile.js', '<%= yeoman.app %>/scripts/**/*.js'],
   test: {
     options: {
-      jshintrc: '<%= yeoman.setup %>/.jshintrc'
+      jshintrc: 'src/.jshintrc'
     },
     src: ['<%= yeoman.test %>/spec/{,*/}*.js']
   }
