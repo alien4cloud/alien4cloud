a4a41d4f89dd185df152e20e03b479f7767dd6d4
Philippe Benoit <philippe.benoit@fastconnect.fr>
2016-03-03 14:14:06 +0100

    Use the 1.1.0 plugins for the tests

--- alien4cloud-rest-it-compatibility/scripts/prepare-compatibility-test.sh
+++ alien4cloud-rest-it-compatibility/scripts/prepare-compatibility-test.sh
@@ -1,11 +1,11 @@
 
 BASEDIR=$1
-VERSION=$2
-PREVIOUS_VERSION=$3
+PREVIOUS_VERSION=$2
 
 echo $BASEDIR
 rm -rf "$BASEDIR/src"
-cp "$BASEDIR/../alien4cloud-mock-paas-provider-plugin/target/alien4cloud-mock-paas-provider-plugin-1.0-$VERSION.zip" "$BASEDIR/../alien4cloud-mock-paas-provider-plugin/target/alien4cloud-mock-paas-provider-plugin-1.0-$PREVIOUS_VERSION.zip"
-cp "$BASEDIR/../alien4cloud-mock-paas-provider-plugin/target/alien4cloud-mock-paas-provider-plugin-1.1-$VERSION.zip" "$BASEDIR/../alien4cloud-mock-paas-provider-plugin/target/alien4cloud-mock-paas-provider-plugin-1.1-$PREVIOUS_VERSION.zip"
-cp "$BASEDIR/../alien4cloud-mock-paas-provider-plugin/target/alien4cloud-mock-paas-provider-plugin-1.1-different-conf-$VERSION.zip" "$BASEDIR/../alien4cloud-mock-paas-provider-plugin/target/alien4cloud-mock-paas-provider-plugin-1.1-different-conf-$PREVIOUS_VERSION.zip"
-cp "$BASEDIR/../alien4cloud-mock-paas-provider-plugin/target/alien4cloud-mock-paas-provider-plugin-1.1-different-conf-$VERSION.zip" "$BASEDIR/../alien4cloud-mock-paas-provider-plugin/target/alien4cloud-mock-paas-provider-plugin-1.1-different-conf-$PREVIOUS_VERSION.zip"
+cp "$BASEDIR/plugins-mock/$PREVIOUS_VERSION/alien4cloud-mock-paas-provider-plugin-1.0-$PREVIOUS_VERSION.zip" "$BASEDIR/../alien4cloud-mock-paas-provider-plugin/target/alien4cloud-mock-paas-provider-plugin-1.0-$PREVIOUS_VERSION.zip"
+cp "$BASEDIR/plugins-mock/$PREVIOUS_VERSION/alien4cloud-mock-paas-provider-plugin-1.1-$PREVIOUS_VERSION.zip" "$BASEDIR/../alien4cloud-mock-paas-provider-plugin/target/alien4cloud-mock-paas-provider-plugin-1.1-$PREVIOUS_VERSION.zip"
+cp "$BASEDIR/plugins-mock/$PREVIOUS_VERSION/alien4cloud-mock-paas-provider-plugin-1.1-different-conf-$PREVIOUS_VERSION.zip" "$BASEDIR/../alien4cloud-mock-paas-provider-plugin/target/alien4cloud-mock-paas-provider-plugin-1.1-different-conf-$PREVIOUS_VERSION.zip"
+cp "$BASEDIR/plugins-mock/$PREVIOUS_VERSION/alien4cloud-mock-paas-provider-plugin-1.1-different-conf-$PREVIOUS_VERSION.zip" "$BASEDIR/../alien4cloud-mock-paas-provider-plugin/target/alien4cloud-mock-paas-provider-plugin-1.1-different-conf-$PREVIOUS_VERSION.zip"
+cp "$BASEDIR/plugins-mock/$PREVIOUS_VERSION/alien4cloud-mock-paas-provider-plugin-invalid-$PREVIOUS_VERSION.zip" "$BASEDIR/../alien4cloud-mock-paas-provider-plugin/target/alien4cloud-mock-paas-provider-plugin-invalid-$PREVIOUS_VERSION.zip"
