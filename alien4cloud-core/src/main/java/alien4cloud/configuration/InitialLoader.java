b426eea6de38f1fe7803d90ef8cb8c84f90168d6
Luc Boutier <luc@fastconnect.fr>
2016-04-04 13:52:54 +0200

    Missing import.

--- alien4cloud-core/src/main/java/alien4cloud/configuration/InitialLoader.java
+++ alien4cloud-core/src/main/java/alien4cloud/configuration/InitialLoader.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collections;
 import java.util.List;
 import java.util.Properties;
 import java.util.Set;
