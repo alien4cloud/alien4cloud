2f11b1f5a60045816bd7bd1f9d55e799108e5210
Luc Boutier <luc@fastconnect.fr>
2016-05-26 12:35:21 +0200

    skeleton of methods

--- alien4cloud-core/src/main/java/org/alien4cloud/tosca/editor/TopologyEditorService.java
+++ alien4cloud-core/src/main/java/org/alien4cloud/tosca/editor/TopologyEditorService.java
@@ -4,5 +4,14 @@ package org.alien4cloud.tosca.editor;
  * This service manages command execution on the TOSCA topology template editor.
  */
 public class TopologyEditorService {
+    // trigger editor operation
+
+    // upload file in the archive
+
+    // git commit
+
+    // git pull
+
+    // full upload of template (or yaml edition)
 
 }
