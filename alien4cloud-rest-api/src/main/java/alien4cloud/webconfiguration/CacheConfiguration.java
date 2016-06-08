cc29d6de07247f0c13cf9300e80ae22949334e4d
Luc Boutier <luc@fastconnect.fr>
2016-06-08 17:37:23 +0200

    Fix image caching configuration.

--- alien4cloud-rest-api/src/main/java/alien4cloud/webconfiguration/CacheConfiguration.java
+++ alien4cloud-rest-api/src/main/java/alien4cloud/webconfiguration/CacheConfiguration.java
@@ -1,15 +1,17 @@
 package alien4cloud.webconfiguration;
 
 import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CacheConfiguration {
+
     @Bean
-    public FilterRegistrationBean metricsFilterRegistration() {
+    public FilterRegistrationBean cacheFilterRegistration(ServletRegistrationBean dispatcherRegistration) {
         FilterRegistrationBean registrationBean = new FilterRegistrationBean(new CacheFilter());
-        registrationBean.addUrlPatterns("/img");
+        registrationBean.addUrlPatterns("/img/*");
         return registrationBean;
     }
 }
