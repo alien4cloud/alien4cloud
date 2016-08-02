5c13e75db9cafc08e76f212f05b96259e86dd4a6
Luc Boutier <luc@fastconnect.fr>
2016-08-02 10:21:48 +0200

    Fix property service.

--- alien4cloud-core/src/main/java/alien4cloud/utils/services/PropertyService.java
+++ alien4cloud-core/src/main/java/alien4cloud/utils/services/PropertyService.java
@@ -15,7 +15,6 @@ import alien4cloud.model.topology.Capability;
 import alien4cloud.model.topology.NodeTemplate;
 import alien4cloud.tosca.properties.constraints.exception.ConstraintValueDoNotMatchPropertyTypeException;
 import alien4cloud.tosca.properties.constraints.exception.ConstraintViolationException;
-import alien4cloud.utils.services.DependencyService.DependencyContext;
 
 /**
  * Service to set and check constraints on properties.
@@ -26,7 +25,7 @@ public class PropertyService {
     private ConstraintPropertyService constraintPropertyService;
 
     public void setPropertyValue(Map<String, AbstractPropertyValue> properties, PropertyDefinition propertyDefinition, String propertyName,
-            Object propertyValue, DependencyContext dependencyContext) throws ConstraintValueDoNotMatchPropertyTypeException, ConstraintViolationException {
+            Object propertyValue) throws ConstraintValueDoNotMatchPropertyTypeException, ConstraintViolationException {
         // take the default value
         if (propertyValue == null) {
             // no check here, the default value has to be valid at parse time
@@ -35,7 +34,7 @@ public class PropertyService {
         }
 
         // if the default value is also empty, we set the property value to null
-        constraintPropertyService.checkPropertyConstraint(propertyName, propertyValue, propertyDefinition, dependencyContext);
+        constraintPropertyService.checkPropertyConstraint(propertyName, propertyValue, propertyDefinition);
         if (propertyValue instanceof String) {
             // constraintPropertyService.checkSimplePropertyConstraint(propertyName, (String) propertyValue, propertyDefinition);
             properties.put(propertyName, new ScalarPropertyValue((String) propertyValue));
@@ -56,12 +55,12 @@ public class PropertyService {
      * @param propertyName the name of the property to set
      * @param propertyValue the value to be set
      */
-    public void setPropertyValue(NodeTemplate nodeTemplate, PropertyDefinition propertyDefinition, String propertyName, Object propertyValue,
-            DependencyContext dependencyContext) throws ConstraintValueDoNotMatchPropertyTypeException, ConstraintViolationException {
+    public void setPropertyValue(NodeTemplate nodeTemplate, PropertyDefinition propertyDefinition, String propertyName, Object propertyValue)
+            throws ConstraintValueDoNotMatchPropertyTypeException, ConstraintViolationException {
         if (nodeTemplate.getProperties() == null) {
             nodeTemplate.setProperties(Maps.<String, AbstractPropertyValue> newHashMap());
         }
-        setPropertyValue(nodeTemplate.getProperties(), propertyDefinition, propertyName, propertyValue, dependencyContext);
+        setPropertyValue(nodeTemplate.getProperties(), propertyDefinition, propertyName, propertyValue);
     }
 
     /**
@@ -72,11 +71,11 @@ public class PropertyService {
      * @param propertyName the name of the property
      * @param propertyValue the value of the property
      */
-    public void setCapabilityPropertyValue(Capability capability, PropertyDefinition propertyDefinition, String propertyName, Object propertyValue,
-            DependencyContext dependencyContext) throws ConstraintValueDoNotMatchPropertyTypeException, ConstraintViolationException {
+    public void setCapabilityPropertyValue(Capability capability, PropertyDefinition propertyDefinition, String propertyName, Object propertyValue)
+            throws ConstraintValueDoNotMatchPropertyTypeException, ConstraintViolationException {
         if (capability.getProperties() == null) {
             capability.setProperties(Maps.<String, AbstractPropertyValue> newHashMap());
         }
-        setPropertyValue(capability.getProperties(), propertyDefinition, propertyName, propertyValue, dependencyContext);
+        setPropertyValue(capability.getProperties(), propertyDefinition, propertyName, propertyValue);
     }
 }
