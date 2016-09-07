4e98cf93d23c3b28da2f549f20ceca53695eda45
Luc Boutier <luc@fastconnect.fr>
2016-09-07 16:16:49 +0200

    Fixing input artifact operations.

---
+++ alien4cloud-core/src/main/java/org/alien4cloud/tosca/editor/processors/inputs/RenameInputArtifactProcessor.java
@@ -0,0 +1,44 @@
+package org.alien4cloud.tosca.editor.processors.inputs;
+
+import static alien4cloud.utils.AlienUtils.safe;
+
+import org.alien4cloud.tosca.editor.EditionContextManager;
+import org.alien4cloud.tosca.editor.operations.inputs.RenameInputArtifactOperation;
+import org.alien4cloud.tosca.editor.processors.IEditorOperationProcessor;
+
+import alien4cloud.exception.AlreadyExistException;
+import alien4cloud.exception.InvalidNameException;
+import alien4cloud.exception.NotFoundException;
+import alien4cloud.model.components.DeploymentArtifact;
+import alien4cloud.model.topology.NodeTemplate;
+import alien4cloud.model.topology.Topology;
+import alien4cloud.utils.InputArtifactUtil;
+import org.springframework.stereotype.Component;
+
+/**
+ * Rename an input artifact.
+ */
+@Component
+public class RenameInputArtifactProcessor implements IEditorOperationProcessor<RenameInputArtifactOperation> {
+    @Override
+    public void process(RenameInputArtifactOperation operation) {
+        Topology topology = EditionContextManager.getTopology();
+        if (operation.getNewInputName() == null || operation.getNewInputName().isEmpty() || !operation.getNewInputName().matches("\\w+")) {
+            throw new InvalidNameException("newInputName", operation.getNewInputName(), "\\w+");
+        }
+        if (safe(topology.getInputArtifacts()).containsKey(operation.getNewInputName())) {
+            throw new AlreadyExistException("Input artifact with name <" + operation.getNewInputName() + "> already exists.");
+        }
+        if (!safe(topology.getInputArtifacts()).containsKey(operation.getInputName())) {
+            throw new NotFoundException("Input artifact with name <" + operation.getInputName() + "> does not exists.");
+        }
+        DeploymentArtifact inputArtifact = topology.getInputArtifacts().remove(operation.getInputName());
+        topology.getInputArtifacts().put(operation.getNewInputName(), inputArtifact);
+        // change the value of concerned node template artifacts
+        for (NodeTemplate nodeTemplate : safe(topology.getNodeTemplates()).values()) {
+            for (DeploymentArtifact dArtifact : safe(nodeTemplate.getArtifacts()).values()) {
+                InputArtifactUtil.updateInputArtifactIdIfNeeded(dArtifact, operation.getInputName(), operation.getNewInputName());
+            }
+        }
+    }
+}
