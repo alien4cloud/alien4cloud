32af7480403032abc1d8a49cab3c08183e94a21b
VU Minh Khang <minh.khang.vu.mkv@gmail.com>
2016-09-21 20:16:51 +0200

    ALIEN-1766 [PREMIUM] As a USER I want to be able to move/copy an archive to another workspace WIP move action

--- alien4cloud-core/src/main/java/org/alien4cloud/tosca/catalog/index/CsarService.java
+++ alien4cloud-core/src/main/java/org/alien4cloud/tosca/catalog/index/CsarService.java
@@ -2,7 +2,14 @@ package org.alien4cloud.tosca.catalog.index;
 
 import static alien4cloud.dao.FilterUtil.fromKeyValueCouples;
 
-import java.util.*;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Queue;
+import java.util.Set;
 
 import javax.annotation.Resource;
 import javax.inject.Inject;
@@ -332,6 +339,27 @@ public class CsarService implements ICsarDependencyLoader {
         }
 
         return relatedResourceList;
+    }
+
+    /**
+     * Get all dependencies of a given CSAR's name and version, transitive dependencies included
+     * 
+     * @param name name of the CSAR
+     * @param version version of the CSAR
+     * @return a set of dependencies for the given CSAR's name and version
+     */
+    public Set<CSARDependency> getAllDependencies(String name, String version) {
+        Set<CSARDependency> result = new HashSet<>();
+        Queue<CSARDependency> queue = new LinkedList<>(getDependencies(name, version));
+        while (!queue.isEmpty()) {
+            CSARDependency dependency = queue.poll();
+            if (result.add(dependency)) {
+                // Add dependencies of the CSAR in the queue if it hasn't been already processed for the first time
+                // This prevent cyclic dependencies
+                queue.addAll(getDependencies(dependency.getName(), dependency.getVersion()));
+            }
+        }
+        return result;
     }
 
     /**
