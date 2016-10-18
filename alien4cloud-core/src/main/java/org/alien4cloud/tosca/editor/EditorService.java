027adcf42d507657aefc881c31bc4983ead1d86f
Philippe Benoit <philippe.benoit@fastconnect.fr>
2016-10-18 11:30:25 +0200

    fixup! ALIEN-1811: Fix a wrong condition in EditorService

--- alien4cloud-core/src/main/java/org/alien4cloud/tosca/editor/EditorService.java
+++ alien4cloud-core/src/main/java/org/alien4cloud/tosca/editor/EditorService.java
@@ -190,7 +190,7 @@ public class EditorService {
         process(operation);
 
         List<AbstractEditorOperation> operations = EditionContextManager.get().getOperations();
-        if (EditionContextManager.get().getLastOperationIndex() != operations.size() - 1 &&  !operations.contains(operation)) {
+        if (EditionContextManager.get().getLastOperationIndex() != operations.size() - 1) {
             // Clear the operations to 'redo'.
             CollectionUtils.clearFrom(operations, EditionContextManager.get().getLastOperationIndex() + 1);
         }
