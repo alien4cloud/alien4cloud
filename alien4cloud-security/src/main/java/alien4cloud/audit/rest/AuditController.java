3d3b8bd58a12842bd3763ecac4e10b639a4a4041
igorn <igor.ngouagna@fastconnect.fr>
2017-01-31 13:38:55 +0100

    secured Audit endpoints

--- alien4cloud-security/src/main/java/alien4cloud/audit/rest/AuditController.java
+++ alien4cloud-security/src/main/java/alien4cloud/audit/rest/AuditController.java
@@ -7,15 +7,20 @@ import java.util.Set;
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 
-import com.google.common.collect.Sets;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 
 import alien4cloud.audit.AuditService;
 import alien4cloud.audit.annotation.Audit;
@@ -119,6 +124,7 @@ public class AuditController {
      */
     @ApiOperation(value = "Search for audit trace", notes = "Returns a search result with that contains auti traces matching the request. Audit search is only accessible to user with role [ ADMIN ]")
     @RequestMapping(value = "/search", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PreAuthorize("hasAuthority('ADMIN')")
     public RestResponse<FacetedSearchResult> search(@RequestBody FilteredSearchRequest searchRequest) {
         FacetedSearchResult searchResult = auditService.searchAuditTrace(searchRequest.getQuery(), searchRequest.getFilters(), searchRequest.getFrom(),
                 searchRequest.getSize());
@@ -130,6 +136,7 @@ public class AuditController {
 
     @ApiOperation(value = "Reset the audit configuration", notes = "Reset the audit configuration to its default state. Audit search is only accessible to user with role [ ADMIN ]")
     @RequestMapping(value = "/configuration/reset", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PreAuthorize("hasAuthority('ADMIN')")
     public RestResponse<AuditConfigurationDTO> generateDefaultConfiguration() {
         AuditConfiguration auditConfiguration = new AuditConfiguration();
         Map<Method, Boolean> allAvailableMethodsForAudit = getAllAvailableMethodsForAudit(requestMappingHandlerMapping);
@@ -143,6 +150,7 @@ public class AuditController {
 
     @ApiOperation(value = "Get audit configuration", notes = "Get the audit configuration object. Audit configuration is only accessible to user with role [ ADMIN ]")
     @RequestMapping(value = "/configuration", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PreAuthorize("hasAuthority('ADMIN')")
     public RestResponse<AuditConfigurationDTO> getAuditConfiguration() {
         AuditConfiguration currentConfiguration = auditService.getMandatoryAuditConfiguration();
         boolean auditEnabled = currentConfiguration.isEnabled();
@@ -161,6 +169,7 @@ public class AuditController {
 
     @ApiOperation(value = "Enable/Disable audit", notes = "Audit configuration update is only accessible to user with role [ ADMIN ]")
     @RequestMapping(value = "/configuration/enabled", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PreAuthorize("hasAuthority('ADMIN')")
     public RestResponse<Void> enableAudit(@RequestParam boolean enabled) {
         AuditConfiguration auditConfiguration = auditService.getMandatoryAuditConfiguration();
         auditConfiguration.setEnabled(enabled);
@@ -181,6 +190,7 @@ public class AuditController {
 
     @ApiOperation(value = "Enable/Disable audit on a list of methods", notes = "Audit configuration update is only accessible to user with role [ ADMIN ]")
     @RequestMapping(value = "/configuration/audited-methods", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PreAuthorize("hasAuthority('ADMIN')")
     public RestResponse<Void> enableMethodAudit(@RequestBody AuditedMethod[] methods) {
         AuditConfiguration auditConfiguration = auditService.getMandatoryAuditConfiguration();
         Map<Method, Boolean> auditedMethodsMap = auditConfiguration.getAuditedMethodsMap();
