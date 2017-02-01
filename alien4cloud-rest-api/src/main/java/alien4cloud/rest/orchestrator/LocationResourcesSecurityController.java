bc62a48717e9f78a0668026bcb4136e6ddf5c707
Philippe Benoit <philippe.benoit@fastconnect.fr>
2017-02-01 18:01:25 +0100

    ALIEN-2062: Add endpoint for applications

--- alien4cloud-rest-api/src/main/java/alien4cloud/rest/orchestrator/LocationResourcesSecurityController.java
+++ alien4cloud-rest-api/src/main/java/alien4cloud/rest/orchestrator/LocationResourcesSecurityController.java
@@ -1,22 +1,31 @@
 package alien4cloud.rest.orchestrator;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import javax.annotation.Resource;
 
+import org.elasticsearch.common.collect.Lists;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+
+import alien4cloud.application.ApplicationEnvironmentService;
 import alien4cloud.audit.annotation.Audit;
 import alien4cloud.dao.IGenericSearchDAO;
-import alien4cloud.model.orchestrators.locations.Location;
+import alien4cloud.model.application.Application;
+import alien4cloud.model.application.ApplicationEnvironment;
 import alien4cloud.model.orchestrators.locations.LocationResourceTemplate;
 import alien4cloud.orchestrators.locations.services.ILocationResourceService;
 import alien4cloud.orchestrators.locations.services.LocationSecurityService;
 import alien4cloud.orchestrators.locations.services.LocationService;
 import alien4cloud.rest.model.RestResponse;
 import alien4cloud.rest.model.RestResponseBuilder;
+import alien4cloud.rest.orchestrator.model.ApplicationEnvironmentAuthorizationDTO;
+import alien4cloud.rest.orchestrator.model.ApplicationEnvironmentAuthorizationUpdateRequest;
 import alien4cloud.rest.orchestrator.model.GroupDTO;
 import alien4cloud.rest.orchestrator.model.UserDTO;
 import alien4cloud.security.ResourcePermissionService;
@@ -41,6 +50,17 @@ public class LocationResourcesSecurityController {
     @Resource
     private ResourcePermissionService resourcePermissionService;
 
+    @Resource
+    private ApplicationEnvironmentService applicationEnvironmentService;
+
+
+    private void hasAuthorizationOrFailed(LocationResourceTemplate resourceTemplate, Subject subject, String[] names) {
+        Map<Subject, Set<String>> subjectsMap = new HashMap<>();
+        subjectsMap.put(subject, Sets.newHashSet(names));
+        if (resourcePermissionService.hasPermission(resourceTemplate, subjectsMap)) {
+            throw new RuntimeException("TODO: improve this exception");
+        }
+    }
 
     /*******************************************************************************************************************************
      *
@@ -62,9 +82,8 @@ public class LocationResourcesSecurityController {
     @Audit
     public synchronized RestResponse<List<UserDTO>> grantAccessToUsers(@PathVariable String orchestratorId, @PathVariable String locationId,
                                                                     @PathVariable String resourceId, @RequestBody String[] userNames) {
-        // TODO check that provided users are authorized on this location before. what to is one of them is not ?
-        // Location location = locationService.getLocation(orchestratorId, locationId);
         LocationResourceTemplate resourceTemplate = locationResourceService.getOrFail(resourceId);
+        hasAuthorizationOrFailed(resourceTemplate, Subject.USER, userNames);
         resourcePermissionService.grantPermission(resourceTemplate, Subject.USER, userNames);
         List<UserDTO> users = LocationSecurityController.convertListUserToListUserDTO(resourcePermissionService.getAuthorizedUsers(resourceTemplate));
         return RestResponseBuilder.<List<UserDTO>> builder().data(users).build();
@@ -83,9 +102,8 @@ public class LocationResourcesSecurityController {
     @Audit
     public synchronized RestResponse<List<UserDTO>> revokeUserAccess(@PathVariable String orchestratorId, @PathVariable String locationId,
                                                                   @PathVariable String resourceId, @PathVariable String username) {
-        // TODO check that provided users are authorized on this location before. what to is one of them is not ?
-        // Location location = locationService.getLocation(orchestratorId, locationId);
         LocationResourceTemplate resourceTemplate = locationResourceService.getOrFail(resourceId);
+        hasAuthorizationOrFailed(resourceTemplate, Subject.USER,  new String[] { username });
         resourcePermissionService.revokePermission(resourceTemplate, Subject.USER, username);
         List<UserDTO> users = LocationSecurityController.convertListUserToListUserDTO(resourcePermissionService.getAuthorizedUsers(resourceTemplate));
         return RestResponseBuilder.<List<UserDTO>> builder().data(users).build();
@@ -124,9 +142,8 @@ public class LocationResourcesSecurityController {
     @Audit
     public synchronized RestResponse<List<GroupDTO>> grantAccessToGroups(@PathVariable String orchestratorId, @PathVariable String locationId,
                                                                          @PathVariable String resourceId, @RequestBody String[] groupIds) {
-        Location location = locationService.getLocation(orchestratorId, locationId);
-        locationSecurityService.checkAuthorisation(location, (String) null);
         LocationResourceTemplate resourceTemplate = locationResourceService.getOrFail(resourceId);
+        hasAuthorizationOrFailed(resourceTemplate, Subject.GROUP, groupIds);
         resourcePermissionService.grantPermission(resourceTemplate, Subject.GROUP, groupIds);
         List<GroupDTO> groups = LocationSecurityController.convertListGroupToListGroupDTO(resourcePermissionService.getAuthorizedGroups(resourceTemplate));
         return RestResponseBuilder.<List<GroupDTO>> builder().data(groups).build();
@@ -145,9 +162,8 @@ public class LocationResourcesSecurityController {
     @Audit
     public synchronized RestResponse<List<GroupDTO>> revokeGroupAccess(@PathVariable String orchestratorId, @PathVariable String locationId,
                                                                        @PathVariable String resourceId, @PathVariable String groupId) {
-        Location location = locationService.getLocation(orchestratorId, locationId);
-        locationSecurityService.checkAuthorisation(location, (String) null);
         LocationResourceTemplate resourceTemplate = locationResourceService.getOrFail(resourceId);
+        hasAuthorizationOrFailed(resourceTemplate, Subject.GROUP, new String[] { groupId });
         resourcePermissionService.revokePermission(resourceTemplate, Subject.GROUP, groupId);
         List<GroupDTO> groups = LocationSecurityController.convertListGroupToListGroupDTO(resourcePermissionService.getAuthorizedGroups(resourceTemplate));
         return RestResponseBuilder.<List<GroupDTO>> builder().data(groups).build();
@@ -162,12 +178,150 @@ public class LocationResourcesSecurityController {
     @RequestMapping(value = "/groups", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @PreAuthorize("hasAuthority('ADMIN')")
     public RestResponse<List<GroupDTO>> getAuthorizedGroups(@PathVariable String orchestratorId, @PathVariable String locationId, @PathVariable String resourceId) {
-        Location location = locationService.getLocation(orchestratorId, locationId);
-        locationSecurityService.checkAuthorisation(location, (String) null);
         LocationResourceTemplate resourceTemplate = locationResourceService.getOrFail(resourceId);
         List<GroupDTO> groups = LocationSecurityController.convertListGroupToListGroupDTO(resourcePermissionService.getAuthorizedGroups(resourceTemplate));
         return RestResponseBuilder.<List<GroupDTO>> builder().data(groups).build();
     }
 
+
+    /*******************************************************************************************************************************
+     *
+     * SECURITY ON APPLICATIONS
+     *
+     *******************************************************************************************************************************/
+
+    /**
+     * Revoke the application's authorisation to access the location (including all related environments).
+     *
+     * @param locationId The id of the location.
+     * @param applicationId The authorized application.
+     * @return A {@link Void} {@link RestResponse}.
+     */
+    @ApiOperation(value = "Revoke the application's authorisation to access the location", notes = "Only user with ADMIN role can revoke access to the location.")
+    @RequestMapping(value = "/applications/{applicationId}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @Audit
+    public synchronized RestResponse<Void> revokeApplicationAccess(@PathVariable String orchestratorId, @PathVariable String locationId,
+                                                                   @PathVariable String applicationId, @PathVariable String resourceId) {
+        LocationResourceTemplate resourceTemplate = locationResourceService.getOrFail(resourceId);
+        hasAuthorizationOrFailed(resourceTemplate, Subject.APPLICATION, new String[] { applicationId });
+        resourcePermissionService.revokePermission(resourceTemplate, Subject.APPLICATION, applicationId);
+        // remove all environments related to this application
+        ApplicationEnvironment[] aes = applicationEnvironmentService.getByApplicationId(applicationId);
+        String[] envIds = Arrays.stream(aes).map(ae -> ae.getId()).toArray(String[]::new);
+        resourcePermissionService.revokePermission(resourceTemplate, Subject.ENVIRONMENT, envIds);
+        return RestResponseBuilder.<Void>builder().build();
+    }
+
+
+    private void checkAllAuthorizationsForApplicationsAndEnvironments(ApplicationEnvironmentAuthorizationUpdateRequest request, LocationResourceTemplate resource) {
+        if (request.getApplicationsToDelete() != null && request.getApplicationsToDelete().length > 0) {
+            hasAuthorizationOrFailed(resource, Subject.APPLICATION, request.getApplicationsToDelete());
+        }
+        if (request.getEnvironmentsToDelete() != null && request.getEnvironmentsToDelete().length > 0) {
+            hasAuthorizationOrFailed(resource, Subject.ENVIRONMENT, request.getEnvironmentsToDelete());
+        }
+        if (request.getApplicationsToAdd() != null && request.getApplicationsToAdd().length > 0) {
+            hasAuthorizationOrFailed(resource, Subject.APPLICATION, request.getApplicationsToAdd());
+        }
+        if (request.getEnvironmentsToAdd() != null && request.getEnvironmentsToAdd().length > 0) {
+            hasAuthorizationOrFailed(resource, Subject.ENVIRONMENT, request.getEnvironmentsToAdd());
+        }
+    }
+
+    /**
+     * Update applications/environments authorized to access the location.
+     */
+    @ApiOperation(value = "Update applications/environments authorized to access the location", notes = "Only user with ADMIN role can update authorized applications/environments for the location.")
+    @RequestMapping(value = "/environmentsPerApplication", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public synchronized RestResponse<Void> updateAuthorizedEnvironmentsPerApplication(@PathVariable String orchestratorId, @PathVariable String locationId, @PathVariable String resourceId,
+                                                                                      @RequestBody ApplicationEnvironmentAuthorizationUpdateRequest request) {
+        LocationResourceTemplate resourceTemplate = locationResourceService.getOrFail(resourceId);
+        checkAllAuthorizationsForApplicationsAndEnvironments(request, resourceTemplate);
+
+        if (request.getApplicationsToDelete() != null && request.getApplicationsToDelete().length > 0) {
+            resourcePermissionService.revokePermission(resourceTemplate, Subject.APPLICATION, request.getApplicationsToDelete());
+        }
+        if (request.getEnvironmentsToDelete() != null && request.getEnvironmentsToDelete().length > 0) {
+            resourcePermissionService.revokePermission(resourceTemplate, Subject.ENVIRONMENT, request.getEnvironmentsToDelete());
+        }
+        List<String> envIds = Lists.newArrayList();
+        if (request.getApplicationsToAdd() != null && request.getApplicationsToAdd().length > 0) {
+            resourcePermissionService.grantPermission(resourceTemplate, Subject.APPLICATION, request.getApplicationsToAdd());
+            // when an app is added, all eventual existing env authorizations are removed
+            for (String applicationToAddId : request.getApplicationsToAdd()) {
+                ApplicationEnvironment[] aes = applicationEnvironmentService.getByApplicationId(applicationToAddId);
+                for (ApplicationEnvironment ae : aes) {
+                    envIds.add(ae.getId());
+                }
+            }
+            if (!envIds.isEmpty()) {
+                resourcePermissionService.revokePermission(resourceTemplate, Subject.ENVIRONMENT, envIds.toArray(new String[envIds.size()]));
+            }
+        }
+        if (request.getEnvironmentsToAdd() != null && request.getEnvironmentsToAdd().length > 0) {
+            List<String> envToAddSet = Arrays.stream(request.getEnvironmentsToAdd()).filter(env -> !envIds.contains(env)).collect(Collectors.toList());
+            resourcePermissionService.grantPermission(resourceTemplate, Subject.ENVIRONMENT, envToAddSet.toArray(new String[envToAddSet.size()]));
+        }
+        return RestResponseBuilder.<Void> builder().build();
+    }
+
+    /**
+     * List all environments per application authorised to access the location.
+     *
+     * @return list of all environments per application.
+     */
+    @ApiOperation(value = "List all applications/environments authorized to access the location", notes = "Only user with ADMIN role can list authorized applications/environments for the location.")
+    @RequestMapping(value = "/environmentsPerApplication", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public RestResponse<List<ApplicationEnvironmentAuthorizationDTO>> getAuthorizedEnvironmentsPerApplication(@PathVariable String orchestratorId,
+                                                                                                              @PathVariable String locationId, @PathVariable String resourceId) {
+        LocationResourceTemplate resourceTemplate = locationResourceService.getOrFail(resourceId);
+        Map<String, ApplicationEnvironmentAuthorizationDTO> aeaDTOsMap = Maps.newHashMap();
+        if (resourceTemplate.getEnvironmentPermissions() != null && resourceTemplate.getEnvironmentPermissions().size() > 0) {
+
+            // build the set of application ids
+            List<ApplicationEnvironment> environments = alienDAO.findByIds(ApplicationEnvironment.class,
+                    resourceTemplate.getEnvironmentPermissions().keySet().toArray(new String[resourceTemplate.getEnvironmentPermissions().size()]));
+            Set<String> environmentApplicationIds = environments.stream().map(ae -> new String(ae.getApplicationId())).collect(Collectors.toSet());
+
+            // retrieve the applications related to these environments
+            List<Application> applications = alienDAO.findByIds(Application.class,
+                    environmentApplicationIds.toArray(new String[environmentApplicationIds.size()]));
+
+            // for each application, build a DTO
+            for (Application application : applications) {
+                ApplicationEnvironmentAuthorizationDTO dto = new ApplicationEnvironmentAuthorizationDTO();
+                dto.setApplication(application);
+                List<ApplicationEnvironment> aes = Lists.newArrayList();
+                dto.setEnvironments(aes);
+                aeaDTOsMap.put(application.getId(), dto);
+            }
+
+            for (ApplicationEnvironment ae : environments) {
+                ApplicationEnvironmentAuthorizationDTO dto = aeaDTOsMap.get(ae.getApplicationId());
+                dto.getEnvironments().add(ae);
+            }
+        }
+        if (resourceTemplate.getApplicationPermissions() != null && resourceTemplate.getApplicationPermissions().size() > 0) {
+            List<Application> applications = alienDAO.findByIds(Application.class,
+                    resourceTemplate.getApplicationPermissions().keySet().toArray(new String[resourceTemplate.getApplicationPermissions().size()]));
+            for (Application application : applications) {
+                ApplicationEnvironmentAuthorizationDTO dto = aeaDTOsMap.get(application.getId());
+                if (dto == null) {
+                    dto = new ApplicationEnvironmentAuthorizationDTO();
+                    dto.setApplication(application);
+                    aeaDTOsMap.put(application.getId(), dto);
+                } else {
+                    // the application has detailed environment authorizations but the whole application authorization has precedence.
+                    dto.setEnvironments(null);
+                }
+            }
+        }
+
+        List<ApplicationEnvironmentAuthorizationDTO> result = Lists.newArrayList(aeaDTOsMap.values());
+        return RestResponseBuilder.<List<ApplicationEnvironmentAuthorizationDTO>> builder().data(result).build();
+    }
 
 }
