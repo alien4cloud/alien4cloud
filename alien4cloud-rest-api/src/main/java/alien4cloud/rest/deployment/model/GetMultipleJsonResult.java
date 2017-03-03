4e502852baaa20e4c3603ecb8994cafb4bf74ae2
Luc Boutier <luc@fastconnect.fr>
2017-03-03 11:29:44 +0100

    [ALIEN-2187] Add admin oriented event batch processing apis.

---
+++ alien4cloud-rest-api/src/main/java/alien4cloud/rest/deployment/model/GetMultipleJsonResult.java
@@ -0,0 +1,25 @@
+package alien4cloud.rest.deployment.model;
+
+import com.fasterxml.jackson.annotation.JsonRawValue;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Similar to get multiple data result but keep the result as a raw json string to avoid deserialization and serialization.
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor(suppressConstructorProperties = true)
+public class GetMultipleJsonResult {
+    private static final long serialVersionUID = 1L;
+    @JsonRawValue
+    private String data;
+    private long queryDuration;
+    private long totalResults;
+    private int from;
+    private int to;
+}
