f2b1d11b84fdd5dec8317ec965b248305d85d760
Luc Boutier <luc@fastconnect.fr>
2017-03-18 10:01:09 +0100

    Improving build to ensure that every resource is loaded when changing.

---
+++ alien4cloud-ui/src/main/webapp/scripts/alien4cloud-dependencies.js
@@ -0,0 +1,43 @@
+define(function (require) {
+  'use strict';
+
+
+  require('jquery');
+  require('bower_components/jquery-ui/ui/resizable');
+  require('lodash-base');
+  require('angular');
+  require('angular-cookies');
+  require('angular-bootstrap');
+  require('moment');
+  require('angular-bootstrap-datetimepicker-template');
+  require('angular-bootstrap-datetimepicker');
+  require('angular-resource');
+  require('angular-sanitize');
+  require('angular-ui-router');
+  require('angular-translate-base');
+  require('angular-translate');
+  require('angular-animate');
+  require('angular-xeditable');
+  require('angular-ui-select');
+  require('angular-tree-control');
+  require('ng-table');
+  require('autofill-event');
+  require('toaster');
+  require('hopscotch');
+  require('angular-file-upload-shim');
+  require('angular-file-upload');
+  require('angular-ui-ace');
+  require('angular-hotkeys');
+  require('ace');
+  require('sockjs');
+  require('stomp');
+  require('d3');
+  require('d3-tip');
+  require('d3-pie');
+  require('dagre');
+  require('graphlib');
+
+  return function() {
+    console.debug('Dependencies loaded');
+  };
+});
---
+++ alien4cloud-ui/src/main/webapp/views/alien4cloud-templates.js
@@ -0,0 +1,7 @@
+// dev mode file replaced by a registration of all views in the angular template cache (see ngtemplate build task).
+define(function () {
+  'use strict';
+  return function() {
+    console.debug('Development mode, no templates to inject.');
+  };
+});
