fd9da26ac0e40dbc3a137af2be9ab501d6377992
Igor Ngouagna <igor.ngouagna@fastconnect.fr>
2017-06-01 15:10:41 +0200

    ALIEN-2357 fixed broken deployment input properties edition

--- alien4cloud-ui/src/main/webapp/scripts/applications/controllers/application_deployment_input.js
+++ alien4cloud-ui/src/main/webapp/scripts/applications/controllers/application_deployment_input.js
@@ -2,7 +2,7 @@ define(function (require) {
   'use strict';
 
   require('scripts/applications/controllers/application_deployment_input_artifact_modal');
-  
+
   var modules = require('modules');
   var states = require('states');
   var _ = require('lodash');
@@ -32,7 +32,32 @@ define(function (require) {
           $scope.isAllowedInputDeployment = function () {
             return _.isNotEmpty($filter('allowedInputs')(_.get($scope, 'deploymentContext.deploymentTopologyDTO.topology.inputs')));
           };
-      
+
+          /* ******************************************************
+           *      Handle properties inputs
+           **********************************************************/
+          $scope.updateInputValue = function (definition, inputValue, inputId) {
+            // No update if it's the same value
+            if(_.get($scope.deploymentContext.deploymentTopologyDTO.topology.inputProperties[inputId], 'value') === inputValue){
+              return;
+            }
+            var updatedProperties = {};
+            updatedProperties[inputId] = inputValue;
+            return deploymentTopologyServices.updateInputProperties({
+              appId: $scope.application.id,
+              envId: $scope.deploymentContext.selectedEnvironment.id
+            }, angular.toJson({
+              inputProperties: updatedProperties
+            }), function (result) {
+              if (!result.error) {
+                $scope.updateScopeDeploymentTopologyDTO(result.data);
+              }
+            }).$promise;
+          };
+
+          /* ******************************************************
+           *      Handle inputs artifacts
+           **********************************************************/
           $scope.openInputArtifactModal = function (artifactKey, artifact) {
             var key = artifactKey;
             topologyServices.availableRepositories({
@@ -69,19 +94,19 @@ define(function (require) {
                     }
 
                   }
-                })
+                });
 
-                modalInstance.result.then(function (selectedArtifact) {                  
+                modalInstance.result.then(function (selectedArtifact) {
                   if(selectedArtifact){
                     var inputArtifactsDao = $resource('rest/latest/applications/' + $scope.application.id + '/environments/' + $scope.deploymentContext.selectedEnvironment.id + '/deployment-topology/inputArtifacts/' + key + '/update', {}, {
                       'update': {
                         method: 'POST'
                       }
-                    });  
-                    
+                    });
+
                     inputArtifactsDao.update({
                       artifactType: selectedArtifact.artifactType,
-                      artifactName   : selectedArtifact.artifactName, 
+                      artifactName   : selectedArtifact.artifactName,
                       artifactRef: selectedArtifact.reference,
                       artifactRepository : selectedArtifact.repository,
                       archiveName: selectedArtifact.archiveName,
@@ -97,8 +122,8 @@ define(function (require) {
                     });
                   }
               });
-            })
-          };       
+            });
+          };
         } // function
       ]); //controller
 }); //Define
