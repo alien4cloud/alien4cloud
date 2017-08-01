9388e1392d6403ab8934b379cfa18019de0cbd95
VU Minh Khang <minh.khang.vu.mkv@gmail.com>
2017-08-01 13:00:28 +0200

    Fix issue when updating properties of matched nodes.

--- alien4cloud-core/src/main/java/org/alien4cloud/alm/deployment/configuration/flow/modifiers/action/SetMatchedNodePropertyModifier.java
+++ alien4cloud-core/src/main/java/org/alien4cloud/alm/deployment/configuration/flow/modifiers/action/SetMatchedNodePropertyModifier.java
@@ -6,7 +6,6 @@ import java.beans.IntrospectionException;
 import java.util.Map;
 import java.util.Optional;
 
-import alien4cloud.utils.services.ConstraintPropertyService;
 import org.alien4cloud.alm.deployment.configuration.flow.FlowExecutionContext;
 import org.alien4cloud.alm.deployment.configuration.flow.ITopologyModifier;
 import org.alien4cloud.alm.deployment.configuration.flow.modifiers.NodeMatchingConfigAutoSelectModifier;
@@ -26,13 +25,12 @@ import org.alien4cloud.tosca.model.templates.Topology;
 import org.alien4cloud.tosca.model.types.CapabilityType;
 import org.alien4cloud.tosca.model.types.NodeType;
 
-import com.google.common.collect.Maps;
-
 import alien4cloud.exception.NotFoundException;
 import alien4cloud.model.orchestrators.locations.LocationResourceTemplate;
 import alien4cloud.topology.task.LocationPolicyTask;
 import alien4cloud.tosca.context.ToscaContext;
 import alien4cloud.tosca.properties.constraints.ConstraintUtil;
+import alien4cloud.utils.services.ConstraintPropertyService;
 import alien4cloud.utils.services.PropertyService;
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
@@ -107,10 +105,10 @@ public class SetMatchedNodePropertyModifier implements ITopologyModifier {
         NodePropsOverride nodePropsOverride = getNodePropsOverride(matchingConfiguration);
 
         // Perform the update of the property
-        ConstraintPropertyService.checkPropertyConstraint(propertyName, propertyValue, propertyDefinition);
         if (propertyValue == null) {
             nodePropsOverride.getProperties().remove(propertyName);
         } else {
+            ConstraintPropertyService.checkPropertyConstraint(propertyName, propertyValue, propertyDefinition);
             nodePropsOverride.getProperties().put(propertyName, PropertyService.asPropertyValue(propertyValue));
         }
 
