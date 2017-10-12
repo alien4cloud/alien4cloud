94553c34656ba83a1958a0aee8ce9268b4f4ed9b
VU Minh Khang <minh.khang.vu.mkv@gmail.com>
2017-10-12 15:34:58 +0200

    ALIEN-2570 : Handle inline workflow

--- alien4cloud-core/src/main/java/org/alien4cloud/tosca/editor/processors/workflow/RemoveWorkflowProcessor.java
+++ alien4cloud-core/src/main/java/org/alien4cloud/tosca/editor/processors/workflow/RemoveWorkflowProcessor.java
@@ -4,8 +4,10 @@ import org.alien4cloud.tosca.editor.EditionContextManager;
 import org.alien4cloud.tosca.editor.operations.workflow.RemoveWorkflowOperation;
 import org.alien4cloud.tosca.model.templates.Topology;
 import org.alien4cloud.tosca.model.workflow.Workflow;
+import org.alien4cloud.tosca.model.workflow.activities.InlineWorkflowActivity;
 import org.springframework.stereotype.Component;
 
+import alien4cloud.paas.wf.exception.BadWorkflowOperationException;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -22,6 +24,15 @@ public class RemoveWorkflowProcessor extends AbstractWorkflowProcessor<RemoveWor
         ensureNotStandard(workflow, "standard workflow <" + workflow.getName() + "> can not be removed");
         log.debug("removing workflow [ {} ] from topology [ {} ]", operation.getWorkflowName(), topology.getId());
         topology.getWorkflows().remove(operation.getWorkflowName());
+        topology.getWorkflows().values().forEach(wf -> wf.getSteps().values().forEach(step -> {
+            if (step.getActivity() instanceof InlineWorkflowActivity) {
+                InlineWorkflowActivity inlineWorkflowActivity = (InlineWorkflowActivity) step.getActivity();
+                if (inlineWorkflowActivity.getInline().equals(workflow.getName())) {
+                    throw new BadWorkflowOperationException(
+                            "Workflow " + inlineWorkflowActivity.getInline() + " is inlined in workflow " + wf.getName() + " in step " + step.getName());
+                }
+            }
+        }));
     }
 
 }
