8e0938d8c3986a532c160fa125cdfb608c9f25be
Igor Ngouagna <igor.ngouagna@fastconnect.fr>
2017-10-16 15:01:33 +0200

    minor on features files

--- alien4cloud-rest-it/src/test/java/alien4cloud/it/RunApplicationDeploymentIT.java
+++ alien4cloud-rest-it/src/test/java/alien4cloud/it/RunApplicationDeploymentIT.java
@@ -9,7 +9,7 @@ import cucumber.api.junit.Cucumber;
 @CucumberOptions(features = {
         //
         "classpath:alien/rest/application-deployment/"
-        // "classpath:alien/rest/application-deployment/policies/policy_matching.feature"
+        // "classpath:alien/rest/application-deployment/policies_matching/policy_matching.feature"
         // "classpath:alien/rest/application-deployment/service_matching.feature"
         // "classpath:alien/rest/application-deployment/deploy_application.feature"
         // "classpath:alien/rest/application-deployment/deploy_application_with_events.feature"
@@ -18,6 +18,7 @@ import cucumber.api.junit.Cucumber;
         // "classpath:alien/rest/application-deployment/locations/location_matching.feature"
         // "classpath:alien/rest/application-deployment/locations/location_policies_setting.feature"
         // "classpath:alien/rest/application-deployment/nodes_matching/node_substitution.feature"
+        // "classpath:alien/rest/application-deployment/nodes_matching/node_substitution_update.feature"
         // "classpath:alien/rest/application-deployment/undeploy_application.feature"
         //
 }, format = { "pretty", "html:target/cucumber/application_deployment", "json:target/cucumber/cucumber-application-deployment.json" })
