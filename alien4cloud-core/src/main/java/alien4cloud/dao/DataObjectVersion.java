e3b36f76cc9ac17b3a08b124160f7df8f762aec3
Philippe Benoit <philippe.benoit@fastconnect.fr>
2017-10-16 15:29:55 +0200

    Remove wrong ES annotation on DataObjectVersion

--- alien4cloud-core/src/main/java/alien4cloud/dao/DataObjectVersion.java
+++ alien4cloud-core/src/main/java/alien4cloud/dao/DataObjectVersion.java
@@ -1,14 +1,12 @@
 package alien4cloud.dao;
 
+import org.elasticsearch.annotation.ESObject;
+import org.elasticsearch.annotation.Id;
+
 import alien4cloud.utils.VersionUtil;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.elasticsearch.annotation.ESObject;
-import org.elasticsearch.annotation.Id;
-import org.elasticsearch.annotation.query.FetchContext;
-
-import static alien4cloud.dao.model.FetchContext.SUMMARY;
 
 /**
  * DataObjectVersion is used to store the current data version of Alien4Cloud objects (index) for migration purpose.
@@ -23,7 +21,6 @@ public class DataObjectVersion {
     public static final String ID = "alien_data_model";
 
     @Id
-    @FetchContext(contexts = { SUMMARY }, include = { true })
     private String id;
 
     /* Version of the current data in Alien, should be updated by migration plugin after breaking change in the model */
