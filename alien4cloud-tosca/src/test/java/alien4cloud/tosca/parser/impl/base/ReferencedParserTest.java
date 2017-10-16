dd0e25fd7e2d8ebbbeb3d113055c798fbf6de47e
Igor Ngouagna <igor.ngouagna@fastconnect.fr>
2017-10-16 17:34:03 +0200

    fix test

--- alien4cloud-tosca/src/test/java/alien4cloud/tosca/parser/impl/base/ReferencedParserTest.java
+++ alien4cloud-tosca/src/test/java/alien4cloud/tosca/parser/impl/base/ReferencedParserTest.java
@@ -16,19 +16,24 @@ import alien4cloud.tosca.parser.impl.ErrorCode;
 /**
  * Internal junit tests to check yaml mapping framework management for reference parser.
  */
+// @Ignore
 public class ReferencedParserTest {
 
     @Test
     public void referenceToMissingTypeShouldFail() {
         ReferencedParser referencedParser = new ReferencedParser("missingParser");
         ParsingContextExecution contextExecution = new ParsingContextExecution();
-        ParsingContextExecution.init();
-        ParsingContextExecution.setRegistry(Maps.newHashMap());
-        Node node = Mockito.mock(Node.class);
-        Mockito.when(node.getStartMark()).thenReturn(new Mark("name", 0, 10, 10, "", 0));
-        Mockito.when(node.getEndMark()).thenReturn(new Mark("name", 0, 10, 10, "", 0));
-        referencedParser.parse(node, contextExecution);
-        assertEquals(ParsingErrorLevel.ERROR, contextExecution.getParsingErrors().get(0).getErrorLevel());
-        assertEquals(ErrorCode.ALIEN_MAPPING_ERROR, contextExecution.getParsingErrors().get(0).getErrorCode());
+        try {
+            contextExecution.init();
+            contextExecution.setRegistry(Maps.newHashMap());
+            Node node = Mockito.mock(Node.class);
+            Mockito.when(node.getStartMark()).thenReturn(new Mark("name", 0, 10, 10, "", 0));
+            Mockito.when(node.getEndMark()).thenReturn(new Mark("name", 0, 10, 10, "", 0));
+            referencedParser.parse(node, contextExecution);
+            assertEquals(ParsingErrorLevel.ERROR, contextExecution.getParsingErrors().get(0).getErrorLevel());
+            assertEquals(ErrorCode.ALIEN_MAPPING_ERROR, contextExecution.getParsingErrors().get(0).getErrorCode());
+        } finally {
+            ParsingContextExecution.destroy();
+        }
     }
 }
