6985e8ce42f0bc3862e4de4817c2fee7bd2b6eec
VU Minh Khang <minh.khang.vu.mkv@gmail.com>
2017-10-19 14:55:11 +0200

    ALIEN-2596 : (SUPALIEN-997) Memory leak while uploading topology

--- alien4cloud-tosca/src/main/java/alien4cloud/paas/wf/util/WorkflowGraphUtils.java
+++ alien4cloud-tosca/src/main/java/alien4cloud/paas/wf/util/WorkflowGraphUtils.java
@@ -30,9 +30,9 @@ public class WorkflowGraphUtils {
      * <p>
      * Will also detect orphans brothers in the entire graph (cycles not connected to start).
      */
-    public static List<Path> getWorkflowGraphPaths(Workflow workflow) {
+    public static List<Path> getWorkflowGraphCycles(Workflow workflow) {
         // the result
-        List<Path> allPaths = new ArrayList<Path>();
+        List<Path> cycles = new ArrayList<Path>();
         // find the entry steps
         Set<WorkflowStep> graphEntries = getGraphEntrySteps(workflow);
         // all the steps: this set will be cleared while browsing the graph and will able us to detect orphans
@@ -42,7 +42,7 @@ public class WorkflowGraphUtils {
             // we have 1 initial path : it's the 'start'
             Path startPath = new Path();
             initialPaths.add(startPath);
-            recursivelyPopulatePaths(workflow, allPaths, initialPaths, graphEntry, allSteps);
+            recursivelyPopulatePaths(workflow, cycles, initialPaths, graphEntry, allSteps);
         }
         while (!allSteps.isEmpty()) {
             // we have orphans steps that are not on any path, in cycles
@@ -52,15 +52,15 @@ public class WorkflowGraphUtils {
             // initial path
             Path startPath = new Path();
             initialPaths.add(startPath);
-            recursivelyPopulatePaths(workflow, allPaths, initialPaths, step, allSteps);
+            recursivelyPopulatePaths(workflow, cycles, initialPaths, step, allSteps);
         }
-        return allPaths;
+        return cycles;
     }
 
     /**
      * TODO: is this a tail recursive fn ?
      */
-    private static void recursivelyPopulatePaths(Workflow workflow, List<Path> allPaths, List<Path> predecessors, WorkflowStep step,
+    private static void recursivelyPopulatePaths(Workflow workflow, List<Path> cycles, List<Path> predecessors, WorkflowStep step,
             Set<WorkflowStep> allSteps) {
         boolean stepHasFollowers = step.getOnSuccess() != null && !step.getOnSuccess().isEmpty();
         for (Path path : predecessors) {
@@ -68,15 +68,12 @@ public class WorkflowGraphUtils {
                 // the step is already in one of it's parent path, we have a cycle
                 path.setCycle(true);
                 path.setLoopingStep(step);
+                cycles.add(path);
             } else {
                 // a step is added to it's predecessor paths
                 path.add(step);
                 // since the step is on a path, we remove it from the allSteps set
                 allSteps.remove(step);
-            }
-            if (path.isCycle() || !stepHasFollowers) {
-                // this is the end for this path
-                allPaths.add(path);
             }
         }
         if (stepHasFollowers) {
@@ -90,7 +87,7 @@ public class WorkflowGraphUtils {
                     }
                 }
                 if (followersPredecessors.size() > 0) {
-                    recursivelyPopulatePaths(workflow, allPaths, followersPredecessors, followingStep, allSteps);
+                    recursivelyPopulatePaths(workflow, cycles, followersPredecessors, followingStep, allSteps);
                 }
             }
         }
