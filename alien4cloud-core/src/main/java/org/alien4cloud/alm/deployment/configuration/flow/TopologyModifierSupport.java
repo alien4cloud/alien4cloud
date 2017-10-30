990f85a65a1ba92606895c22101ff5710f84532f
Igor Ngouagna <igor.ngouagna@fastconnect.fr>
2017-10-30 11:54:55 +0100

    ALIEN-2583 ALIEN-2592 kubernetes Anti-affinity policy modifer first draft

--- alien4cloud-core/src/main/java/org/alien4cloud/alm/deployment/configuration/flow/TopologyModifierSupport.java
+++ alien4cloud-core/src/main/java/org/alien4cloud/alm/deployment/configuration/flow/TopologyModifierSupport.java
@@ -1,14 +1,14 @@
 package org.alien4cloud.alm.deployment.configuration.flow;
 
-import alien4cloud.application.TopologyCompositionService;
-import alien4cloud.model.common.Tag;
-import alien4cloud.paas.wf.util.WorkflowUtils;
-import alien4cloud.tosca.context.ToscaContext;
-import alien4cloud.utils.AlienUtils;
-import alien4cloud.utils.PropertyUtil;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-import com.google.common.collect.Sets;
+import static alien4cloud.utils.AlienUtils.safe;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import javax.annotation.Resource;
+
 import org.alien4cloud.tosca.editor.operations.nodetemplate.AddNodeOperation;
 import org.alien4cloud.tosca.editor.operations.nodetemplate.ReplaceNodeOperation;
 import org.alien4cloud.tosca.editor.operations.nodetemplate.UpdateNodePropertyValueOperation;
@@ -21,18 +21,26 @@ import org.alien4cloud.tosca.model.Csar;
 import org.alien4cloud.tosca.model.definitions.AbstractPropertyValue;
 import org.alien4cloud.tosca.model.definitions.ComplexPropertyValue;
 import org.alien4cloud.tosca.model.definitions.ListPropertyValue;
-import org.alien4cloud.tosca.model.definitions.ScalarPropertyValue;
-import org.alien4cloud.tosca.model.templates.*;
+import org.alien4cloud.tosca.model.templates.AbstractTemplate;
+import org.alien4cloud.tosca.model.templates.Capability;
+import org.alien4cloud.tosca.model.templates.NodeTemplate;
+import org.alien4cloud.tosca.model.templates.PolicyTemplate;
+import org.alien4cloud.tosca.model.templates.RelationshipTemplate;
+import org.alien4cloud.tosca.model.templates.Topology;
 import org.alien4cloud.tosca.model.types.NodeType;
 import org.alien4cloud.tosca.model.types.RelationshipType;
 import org.alien4cloud.tosca.normative.constants.NormativeRelationshipConstants;
+import org.apache.commons.collections4.CollectionUtils;
 
-import javax.annotation.Resource;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+
+import alien4cloud.application.TopologyCompositionService;
+import alien4cloud.model.common.Tag;
+import alien4cloud.paas.wf.util.WorkflowUtils;
+import alien4cloud.tosca.context.ToscaContext;
+import alien4cloud.utils.PropertyUtil;
 
 /**
  * Base class for topology modifiers that can helps adding nodes, setting properties, replacing nodes, adding relationships.
@@ -112,7 +120,7 @@ public abstract class TopologyModifierSupport implements ITopologyModifier {
     @Deprecated
     public static Set<NodeTemplate> getNodesOfType(Topology topology, String type, boolean manageInheritance) {
         Set<NodeTemplate> result = Sets.newHashSet();
-        for (NodeTemplate nodeTemplate : AlienUtils.safe(topology.getNodeTemplates()).values()) {
+        for (NodeTemplate nodeTemplate : safe(topology.getNodeTemplates()).values()) {
             if (nodeTemplate.getType().equals(type)) {
                 result.add(nodeTemplate);
             } else if (manageInheritance) {
@@ -129,10 +137,23 @@ public abstract class TopologyModifierSupport implements ITopologyModifier {
      * Get the policies that target this node template.
      */
     public static Set<PolicyTemplate> getTargetedPolicies(Topology topology, NodeTemplate nodeTemplate) {
-        return AlienUtils.safe(topology.getPolicies()).values().stream()
+        return safe(topology.getPolicies()).values().stream()
                 .filter(policyTemplate -> policyTemplate.getTargets() != null
                         && policyTemplate.getTargets().contains(nodeTemplate.getName()))
                 .collect(Collectors.toSet());
+    }
+
+    /**
+     * Get the members targeted by this policy.
+     */
+    public static Set<NodeTemplate> getTargetedMembers(Topology topology, PolicyTemplate policyTemplate) {
+        return CollectionUtils.isEmpty(policyTemplate.getTargets()) ? Sets.newHashSet()
+                : safe(topology.getNodeTemplates()).values().stream().filter(nodeTemplate -> isPolicyTarget(nodeTemplate, policyTemplate))
+                        .collect(Collectors.toSet());
+    }
+
+    private static boolean isPolicyTarget(NodeTemplate nodeTemplate, PolicyTemplate policyTemplate) {
+        return safe(policyTemplate.getTargets()).contains(nodeTemplate.getName());
     }
 
     /**
@@ -173,7 +194,7 @@ public abstract class TopologyModifierSupport implements ITopologyModifier {
     @Deprecated
     public static Set<NodeTemplate> getTargetNodes(Topology topology, NodeTemplate nodeTemplate, String requirementName) {
         Set<NodeTemplate> result = Sets.newHashSet();
-        for (RelationshipTemplate relationshipTemplate : AlienUtils.safe(nodeTemplate.getRelationships()).values()) {
+        for (RelationshipTemplate relationshipTemplate : safe(nodeTemplate.getRelationships()).values()) {
             if (relationshipTemplate.getRequirementName().equals(requirementName)) {
                 result.add(topology.getNodeTemplates().get(relationshipTemplate.getTarget()));
             }
@@ -189,7 +210,7 @@ public abstract class TopologyModifierSupport implements ITopologyModifier {
     public static Set<NodeTemplate> getSourceNodes(Topology topology, NodeTemplate nodeTemplate, String capabilityName) {
         Set<NodeTemplate> result = Sets.newHashSet();
         for (NodeTemplate node : topology.getNodeTemplates().values()) {
-            for (RelationshipTemplate relationshipTemplate : AlienUtils.safe(node.getRelationships()).values()) {
+            for (RelationshipTemplate relationshipTemplate : safe(node.getRelationships()).values()) {
                 if (relationshipTemplate.getTargetedCapabilityName().equals(capabilityName) && relationshipTemplate.getTarget().equals(nodeTemplate.getName())) {
                     result.add(node);
                 }
@@ -207,7 +228,8 @@ public abstract class TopologyModifierSupport implements ITopologyModifier {
         addRelationshipOperation.setRelationshipVersion(relationshipType.getArchiveVersion());
         addRelationshipOperation.setRequirementName(requirementName);
         addRelationshipOperation.setTargetedCapabilityName(capabilityName);
-        String relationShipName = TopologyCompositionService.ensureNodeNameIsUnique(AlienUtils.safe(sourceNode.getRelationships()).keySet(), sourceNode.getName() + "_" + targetNodeName, 0);
+        String relationShipName = TopologyCompositionService.ensureNodeNameIsUnique(safe(sourceNode.getRelationships()).keySet(),
+                sourceNode.getName() + "_" + targetNodeName, 0);
         addRelationshipOperation.setRelationshipName(relationShipName);
         addRelationshipProcessor.process(csar, topology, addRelationshipOperation);
         return sourceNode.getRelationships().get(relationShipName);
@@ -224,7 +246,7 @@ public abstract class TopologyModifierSupport implements ITopologyModifier {
     // TODO ALIEN-2589: move elsewhere
     @Deprecated
     public static boolean hasRelationship(NodeTemplate sourceNode, String targetNodeName, String requirementName, String capabilityName) {
-        for (RelationshipTemplate relationshipTemplate : AlienUtils.safe(sourceNode.getRelationships()).values()) {
+        for (RelationshipTemplate relationshipTemplate : safe(sourceNode.getRelationships()).values()) {
             if (relationshipTemplate.getTarget().equals(targetNodeName) && relationshipTemplate.getRequirementName().equals(requirementName) && relationshipTemplate.getTargetedCapabilityName().equals(capabilityName)) {
                 return true;
             }
@@ -235,9 +257,9 @@ public abstract class TopologyModifierSupport implements ITopologyModifier {
     // TODO ALIEN-2589: move elsewhere
     @Deprecated
     public static AbstractPropertyValue getNodeCapabilityPropertyValue(NodeTemplate nodeTemplate, String capabilityName, String propertyPath) {
-        Capability capability = AlienUtils.safe(nodeTemplate.getCapabilities()).get(capabilityName);
+        Capability capability = safe(nodeTemplate.getCapabilities()).get(capabilityName);
         if (capability != null) {
-            return PropertyUtil.getPropertyValueFromPath(AlienUtils.safe(capability.getProperties()), propertyPath);
+            return PropertyUtil.getPropertyValueFromPath(safe(capability.getProperties()), propertyPath);
         }
         return null;
     }
@@ -337,10 +359,13 @@ public abstract class TopologyModifierSupport implements ITopologyModifier {
             Map<String, Object> currentMap = null;
             for (int i = 0; i < paths.length; i++) {
                 if (i == 0) {
-                    AbstractPropertyValue currentPropertyValue = propertyValues.get(paths[i]);
+                    AbstractPropertyValue currentPropertyValue = propertyValues.get(paths[i]); // path[i] == nodePropertyName
                     if (currentPropertyValue != null && currentPropertyValue instanceof ComplexPropertyValue) {
                         currentMap = ((ComplexPropertyValue) currentPropertyValue).getValue();
                     } else {
+                        // FIXME OVERRIDING PROP VALUE This overrides the nodePropertyName property value!!!. We should instead fail if currentPropertyValue not
+                        // instanceof ComplexPropertyValue
+                        // FIXME and do this only if currentPropertyValue is null
                         currentMap = Maps.newHashMap();
                         propertyValues.put(nodePropertyName, new ComplexPropertyValue(currentMap));
                     }
@@ -353,6 +378,7 @@ public abstract class TopologyModifierSupport implements ITopologyModifier {
                         if (currentEntry != null && currentEntry instanceof ListPropertyValue) {
                             listPropertyValue = (ListPropertyValue) currentEntry;
                         } else {
+                            // FIXME Same as OVERRIDING PROP VALUE above
                             listPropertyValue = new ListPropertyValue(Lists.newArrayList());
                             currentMap.put(paths[i], listPropertyValue);
                         }
@@ -366,6 +392,7 @@ public abstract class TopologyModifierSupport implements ITopologyModifier {
                     if (currentPropertyValueObj != null && currentPropertyValueObj instanceof Map<?, ?>) {
                         currentPropertyValue = (Map<String, Object>) currentPropertyValueObj;
                     } else {
+                        // FIXME Same as OVERRIDING PROP VALUE above
                         currentPropertyValue = Maps.newHashMap();
                         currentMap.put(paths[i], currentPropertyValue);
                     }
