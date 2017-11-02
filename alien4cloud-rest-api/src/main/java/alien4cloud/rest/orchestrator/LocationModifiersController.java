3bd25ea26a8cde2f6ae1ed4f31076cd727e78f10
Luc Boutier <luc@fastconnect.fr>
2017-11-02 11:49:10 +0100

    [ALIEN-2567] Fix controller.

--- alien4cloud-rest-api/src/main/java/alien4cloud/rest/orchestrator/LocationModifiersController.java
+++ alien4cloud-rest-api/src/main/java/alien4cloud/rest/orchestrator/LocationModifiersController.java
@@ -1,18 +1,9 @@
 package alien4cloud.rest.orchestrator;
 
-import alien4cloud.audit.annotation.Audit;
-import alien4cloud.model.orchestrators.locations.Location;
-import alien4cloud.model.orchestrators.locations.LocationModifierReference;
-import alien4cloud.orchestrators.locations.services.LocationModifierService;
-import alien4cloud.orchestrators.locations.services.LocationService;
-import alien4cloud.rest.model.RestResponse;
-import alien4cloud.rest.model.RestResponseBuilder;
-import com.google.common.collect.Lists;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.Authorization;
-import lombok.extern.slf4j.Slf4j;
+import java.util.List;
+
+import javax.annotation.Resource;
+
 import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,9 +12,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
-import java.util.List;
-import java.util.Set;
+import com.google.common.collect.Lists;
+
+import alien4cloud.audit.annotation.Audit;
+import alien4cloud.model.orchestrators.locations.Location;
+import alien4cloud.model.orchestrators.locations.LocationModifierReference;
+import alien4cloud.orchestrators.locations.services.LocationModifierService;
+import alien4cloud.orchestrators.locations.services.LocationService;
+import alien4cloud.rest.model.RestResponse;
+import alien4cloud.rest.model.RestResponseBuilder;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.Authorization;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Manages the location modifiers associated with the location.
@@ -45,28 +47,29 @@ public class LocationModifiersController {
     @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     @PreAuthorize("hasAuthority('ADMIN')")
     @Audit
-    public RestResponse<Boolean> add(@ApiParam(value = "Id of the location", required = true) @PathVariable String locationId,
-                                     @ApiParam(value = "The location modifier to add", required = true) @RequestBody LocationModifierReference locationModifierReference) {
+    public RestResponse<Void> add(@ApiParam(value = "Id of the location", required = true) @PathVariable String locationId,
+            @ApiParam(value = "The location modifier to add", required = true) @RequestBody LocationModifierReference locationModifierReference) {
         Location location = locationService.getOrFail(locationId);
-        boolean added = locationModifierService.add(location, locationModifierReference);
-        return RestResponseBuilder.<Boolean> builder().data(added).build();
+        locationModifierService.add(location, locationModifierReference);
+        return RestResponseBuilder.<Void> builder().build();
     }
 
     @ApiOperation(value = "Update the order of a modifier.", authorizations = { @Authorization("ADMIN") })
     @RequestMapping(value = "/from/{from}/to/{to}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
-    public RestResponse<Boolean> move(@ApiParam(value = "Id of the location", required = true) @PathVariable String locationId,
-                                        @ApiParam(value = "From index", required = true) @PathVariable int from, @ApiParam(value = "To index", required = true) @PathVariable int to) {
+    public RestResponse<Void> move(@ApiParam(value = "Id of the location", required = true) @PathVariable String locationId,
+            @ApiParam(value = "From index", required = true) @PathVariable int from, @ApiParam(value = "To index", required = true) @PathVariable int to) {
         Location location = locationService.getOrFail(locationId);
-        boolean moved = locationModifierService.move(location, from, to);
-        return RestResponseBuilder.<Boolean> builder().data(moved).build();
+        locationModifierService.move(location, from, to);
+        return RestResponseBuilder.<Void> builder().build();
     }
 
     @ApiOperation(value = "Delete a location modifier at the given index.", authorizations = { @Authorization("ADMIN") })
     @RequestMapping(value = "/{index}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public RestResponse<Boolean> delete(@ApiParam(value = "Id of the location", required = true) @PathVariable String locationId, @ApiParam(value = "Index of the location modifier to delete", required = true) @PathVariable int index) {
+    public RestResponse<Void> delete(@ApiParam(value = "Id of the location", required = true) @PathVariable String locationId,
+            @ApiParam(value = "Index of the location modifier to delete", required = true) @PathVariable int index) {
         Location location = locationService.getOrFail(locationId);
-        boolean removed = locationModifierService.remove(location, index);
-        return RestResponseBuilder.<Boolean> builder().data(removed).build();
+        locationModifierService.remove(location, index);
+        return RestResponseBuilder.<Void> builder().build();
     }
 
     @ApiOperation(value = "Get all modifiers for a given location.")
