7173924d235dbf6fbc7360186efbdd534be6cdd0
Luc Boutier <luc@fastconnect.fr>
2017-11-12 08:51:18 +0100

    [ALIEN-2640] Fix clone of node filters (preventing node type cloning).

--- alien4cloud-tosca/src/main/java/org/alien4cloud/tosca/model/definitions/FilterDefinition.java
+++ alien4cloud-tosca/src/main/java/org/alien4cloud/tosca/model/definitions/FilterDefinition.java
@@ -3,16 +3,19 @@ package org.alien4cloud.tosca.model.definitions;
 import java.util.List;
 import java.util.Map;
 
+import alien4cloud.json.deserializer.PropertyConstraintListDeserializer;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.google.common.collect.Maps;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import com.google.common.collect.Maps;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class FilterDefinition {
     /** Property constraint list by property */
+    @JsonDeserialize(contentUsing = PropertyConstraintListDeserializer.class)
     private Map<String, List<PropertyConstraint>> properties = Maps.newHashMap();
 }
