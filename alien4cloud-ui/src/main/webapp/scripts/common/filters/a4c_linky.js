b6590b31c7435046500135acd9ea93ed6febb0ac
Igor Ngouagna <igor.ngouagna@fastconnect.fr>
2017-12-06 18:41:43 +0100

    ALIEN-2700 ALIEN-2723 display referenced variables in an input expression as a link

---
+++ alien4cloud-ui/src/main/webapp/scripts/common/filters/a4c_linky.js
@@ -0,0 +1,101 @@
+define(function (require) {
+  'use strict';
+
+  var modules = require('modules');
+  var _ = require('lodash');
+  var alienCommonModule = modules.get('a4c-common');
+  var angular = require('angular');
+
+  /**
+  **
+  ** This filter returns a HTML String.
+  ** You might want to use this with a4cCompile directive like this:
+  ** <span a4c-compile="text | a4cLinky:'clickCallbackName'"></span>
+  **
+  **/
+  alienCommonModule.filter('a4cLinky', [ function() {
+
+    //TODO make this REGEXP overridable via filter params
+    var REGEXP = /\$\{([^}]+)\}/;
+
+    var linkyMinErr = angular.$$minErr('a4cLinky');
+
+    /**
+    ** @param text: The text to linkify
+    ** @param clickCallbackName: name of the ng-click callback. Should take a String representing the matched expression to link
+    ** @param attributes: object ==> key:value or attributes to add to the <a></a> links
+    ** attributes could also be a function. In that case, will take the matched expression as param
+    **
+    **/
+    return function(text, clickCallbackName, attributes) {
+      if (_.undefined(text) || text === '') {return text;}
+      if (!_.isString(text)) {throw linkyMinErr('notstring', 'Expected string but received: {0}', text);}
+
+      var attributesFn =
+        _.isFunction(attributes) ? attributes :
+        _.isObject(attributes) ? function getAttributesObject() {return attributes;} :
+        function getEmptyAttributesObject() {return {};};
+
+      var match;
+      var raw = text;
+      var html = [];
+      var i;
+
+      function addText(text) {
+        if (!text) {
+          return;
+        }
+        html.push(text);
+      }
+
+      function addLink(matched, text) {
+        var key, linkAttributes = attributesFn(matched);
+        html.push('<a ');
+
+        for (key in linkAttributes) {
+          html.push(key + '="' + linkAttributes[key] + '" ');
+        }
+
+        html.push('ng-click="',
+                  clickCallbackName,
+                  '(\'',
+                  matched,
+                  '\')',
+                  '">');
+        addText(text);
+        html.push('</a>');
+      }
+      while ((match = raw.match(REGEXP))) {
+        // We can not end in these as they are sometimes found at the end of the sentence
+        i = match.index;
+        addText(raw.substr(0, i));
+        addLink(match[1], match[0]);
+        raw = raw.substring(i + match[0].length);
+      }
+      addText(raw);
+      return html.join('');
+    };
+  }]);
+
+  alienCommonModule.directive('a4cCompile', ['$compile', function ($compile) {
+    return function(scope, element, attrs) {
+         scope.$watch(
+          function(scope) {
+             // watch the 'a4c-compile' expression for changes
+            return scope.$eval(attrs.a4cCompile);
+          },
+          function(value) {
+            // when the 'a4c-compile' expression changes
+            // assign it into the current DOM
+            element.html(value);
+
+            // compile the new DOM and link it to the current
+            // scope.
+            // NOTE: we only compile .childNodes so that
+            // we don't get into infinite loop compiling ourselves
+            $compile(element.contents())(scope);
+          }
+      );
+    };
+  }]);
+});
--- alien4cloud-ui/src/main/webapp/scripts/topology/directives/editor_inputs.js
+++ alien4cloud-ui/src/main/webapp/scripts/topology/directives/editor_inputs.js
@@ -8,6 +8,7 @@ define(function (require) {
 
   require('scripts/topology/services/topology_variables_service');
   require('scripts/topology/services/topology_browser_service');
+  require('scripts/common/filters/a4c_linky');
 
   modules.get('a4c-topology-editor').directive('editorInputs',
     [
@@ -22,13 +23,11 @@ define(function (require) {
 
 
   modules.get('a4c-topology-editor', ['a4c-common', 'ui.ace', 'treeControl']).controller('editorInputsCtrl',
-    ['$scope', 'topologyVariableService', '$http', 'topoBrowserService', function($scope, topologyVariableService, $http, topoBrowserService) {
+    ['$scope', 'topologyVariableService', '$http', 'topoBrowserService', '$filter', function($scope, topologyVariableService, $http, topoBrowserService, $filter) {
 
-      //return if no inputs defined in topology
-      if(_.isEmpty($scope.topology.topology.inputs)){
-        return;
-      }
-      $scope.jsYml = yaml;
+      $scope.dump = function(value) {
+        return $filter('a4cLinky')(_.trim(yaml.safeDump(value, {indent: 4}), '"\n'), 'openVarModal');
+      };
 
       function refresh(expanded){
         var inputsFileNode = topologyVariableService.getInputs(expanded);
@@ -40,6 +39,7 @@ define(function (require) {
           });
         }
       }
+
       function updateInputFile(content) {
         $scope.execute({
           type: 'org.alien4cloud.tosca.editor.operations.UpdateFileContentOperation',
@@ -51,10 +51,16 @@ define(function (require) {
       $scope.clearInput = function(inputName) {
         if(_.has($scope.loadedInputs, inputName)){
           delete $scope.loadedInputs[inputName];
+          var content = yaml.safeDump($scope.loadedInputs);
+          updateInputFile(content);
         }
-        var content = yaml.safeDump($scope.loadedInputs || {});
-        updateInputFile(content);
       };
+
+      $scope.openVarModal = function(varName){
+        console.log('clicked on var ==>', varName);
+      };
+
+
       var firstLoad = true;
       $scope.$watch('topology.archiveContentTree.children[0]', function(newValue, oldValue){
         if(_.defined(newValue) && (firstLoad || newValue !== oldValue)){
