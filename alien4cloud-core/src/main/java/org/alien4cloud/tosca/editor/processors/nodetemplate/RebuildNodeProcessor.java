63938c377afa4e3a22e07f6bf7e58e8b482f4f84
Sébastien LAURENT <sebastien.laurent@atos.net>
2018-10-11 15:09:53 +0200

    Report [ALIEN-2987] Fix for topology recovery
    Report des commits 2b12513 et 7970332

--- alien4cloud-core/src/main/java/org/alien4cloud/tosca/editor/processors/nodetemplate/RebuildNodeProcessor.java
+++ alien4cloud-core/src/main/java/org/alien4cloud/tosca/editor/processors/nodetemplate/RebuildNodeProcessor.java
@@ -28,8 +28,14 @@ public class RebuildNodeProcessor extends AbstractNodeProcessor<RebuildNodeOpera
         // Artifacts are copied from the type to the template
         // In case of an update of version, we must remove old artifacts copied from old types
         // FIXME This is very tricky, we must think about stopping copying artifact from types to templates
-        nodeTemplate.getArtifacts().entrySet().removeIf(artifactEntry -> Objects.equals(type.getArchiveName(), artifactEntry.getValue().getArchiveName())
-                && !Objects.equals(type.getArchiveVersion(), artifactEntry.getValue().getArchiveVersion()));
+        nodeTemplate.getArtifacts().entrySet().removeIf(artifactEntry -> Objects.equals(type.getArchiveName(), artifactEntry.getValue().getArchiveName()));
+
+        // We need to do this on the whole hierarchy
+        for (String typeName : type.getDerivedFrom()) {
+            NodeType subType = ToscaContext.getOrFail(NodeType.class, typeName);
+            nodeTemplate.getArtifacts().entrySet().removeIf(artifactEntry -> Objects.equals(subType.getArchiveName(), artifactEntry.getValue().getArchiveName()));
+        }
+
         NodeTemplate rebuiltNodeTemplate = TemplateBuilder.buildNodeTemplate(type, nodeTemplate);
         rebuiltNodeTemplate.setName(operation.getNodeName());
         topology.getNodeTemplates().put(operation.getNodeName(), rebuiltNodeTemplate);
