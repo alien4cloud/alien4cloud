0392ebf9bb6e8ba21629245486d92c0a614013c3
Xavier DEGENNE <xavier.degenne@gmail.com>
2019-07-09 15:38:26 +0200

    Add JWT authentication endpoint and request filter

---
+++ alien4cloud-security/src/main/java/alien4cloud/security/users/JwtTokenService.java
@@ -0,0 +1,103 @@
+package alien4cloud.security.users;
+
+import alien4cloud.security.AuthorizationUtil;
+import alien4cloud.security.model.User;
+import com.google.common.collect.Sets;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.impl.DefaultClaims;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * A service that manages JWT tokens.
+ */
+@Service
+public class JwtTokenService {
+
+    private static final String USER_SECRET = "userSecret";
+
+    /**
+     * A secret that is used to
+     */
+    @Value("${jwt.token.secret:my_app_secret}")
+    private String secret;
+
+    /**
+     * In hours, the duration of the availability for a JWT token.
+     */
+    @Value("${jwt.token.ttlInHours:24}")
+    private Integer tokenTtlInHours;
+
+    public String createTokens(Authentication authentication) {
+        User user = (User) authentication.getPrincipal();
+        String token = createToken(user);
+        return token;
+    }
+
+    public String createToken(User user) {
+        return Jwts.builder()
+                .signWith(SignatureAlgorithm.HS512, secret)
+                .setClaims(buildUserClaims(user))
+                .setExpiration(getTokenExpirationDate(false))
+                .setIssuedAt(new Date())
+                .compact();
+    }
+
+    public Jws<Claims> validateJwtToken(String token) {
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
+    }
+
+    private Date getTokenExpirationDate(boolean refreshToken) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.MONTH, tokenTtlInHours);
+        return calendar.getTime();
+    }
+
+    private Claims buildUserClaims(User user) {
+        Claims claims = new DefaultClaims();
+
+        claims.setSubject(String.valueOf(user.getUserId()));
+        claims.put("username", user.getUsername());
+        claims.put("email", user.getEmail());
+        if (user.getRoles() != null) {
+            claims.put("roles", String.join(",", user.getRoles()));
+        }
+        if (user.getGroupRoles() != null) {
+            claims.put("groupRoles", String.join(",", user.getGroupRoles()));
+        }
+        // We use the encoded password as user salt
+        claims.put(USER_SECRET, user.getPassword());
+
+        return claims;
+    }
+
+    public Authentication buildAuthenticationFromClaim(Jws<Claims> claims) {
+        String username = claims.getBody().get("username").toString();
+        String email = claims.getBody().get("email").toString();
+
+        String password = claims.getBody().get(USER_SECRET).toString();
+
+        User user = new User();
+        user.setUsername(username);
+        user.setEmail(email);
+        if (claims.getBody().containsKey("roles")) {
+            String rolesStr = claims.getBody().get("roles").toString();
+            user.setRoles(rolesStr.split(","));
+        }
+        if (claims.getBody().containsKey("groupRoles")) {
+            String groupRolesStr = claims.getBody().get("groupRoles").toString();
+            user.setGroupRoles(Sets.newHashSet(groupRolesStr.split(",")));
+        }
+
+        // FIXME : we a database call here, for each request !!
+        return AuthorizationUtil.createAuthenticationToken(user, password);
+    }
+
+}
---
+++ alien4cloud-security/src/main/java/alien4cloud/security/users/rest/JwtAuthenticationTokenFilter.java
@@ -0,0 +1,69 @@
+package alien4cloud.security.users.rest;
+
+import alien4cloud.security.users.JwtTokenService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Optional;
+
+/**
+ * A filter that checks if a JWT authentication token is present in the header. If found, it's verified.
+ */
+@Component
+public class JwtAuthenticationTokenFilter extends GenericFilterBean {
+
+    private static final String BEARER = "Bearer";
+
+    @Autowired
+    private JwtTokenService jwtTokenService;
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
+
+        final HttpServletRequest request = (HttpServletRequest) servletRequest;
+        final HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        // Assume we have only one Authorization header value
+        final Optional<String> token = Optional.ofNullable(request.getHeader(HttpHeaders.AUTHORIZATION));
+
+        Authentication authentication;
+
+        if(token.isPresent() && token.get().startsWith(BEARER)) {
+
+            String bearerToken = token.get().substring(BEARER.length()+1);
+
+            try {
+                Jws<Claims> claims = jwtTokenService.validateJwtToken(bearerToken);
+                authentication = jwtTokenService.buildAuthenticationFromClaim(claims);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } catch (ExpiredJwtException exception) {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "error.jwt.expired");
+                return;
+            } catch (JwtException exception) {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "error.jwt.invalid");
+                return;
+            }
+
+        }
+
+        chain.doFilter(servletRequest, servletResponse);
+
+    }
+
+
+}
