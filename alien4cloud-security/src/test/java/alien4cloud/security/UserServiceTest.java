afc9e93fe48c43891069cee676a015336bd013e7
OrDTesters <ordtesters@gmail.com>
2019-08-03 16:24:05 -0500

    Moving reset logic into @After cleanup method so later tests run are not affected by changes to state

--- alien4cloud-security/src/test/java/alien4cloud/security/UserServiceTest.java
+++ alien4cloud-security/src/test/java/alien4cloud/security/UserServiceTest.java
@@ -8,6 +8,7 @@ import alien4cloud.security.model.User;
 import alien4cloud.security.users.UserService;
 import alien4cloud.security.users.IAlienUserDao;
 import org.junit.FixMethodOrder;
+import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
@@ -34,7 +35,6 @@ public class UserServiceTest {
 
     @Test
     public void testEnsureAdminUserShouldNotCreateUser() throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException {
-        Mockito.reset(alienUserDao);
         enableEnsure();
         GetMultipleDataResult searchResult = new GetMultipleDataResult(null, null, 0, 1, 0, 1);
         Mockito.when(alienUserDao.find(Mockito.anyMap(), Mockito.eq(1))).thenReturn(searchResult);
@@ -45,12 +45,16 @@ public class UserServiceTest {
 
     @Test
     public void testEnsureAdminUserShouldCreateUser() throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException {
-        Mockito.reset(alienUserDao);
         enableEnsure();
         GetMultipleDataResult searchResult = new GetMultipleDataResult(null, null, 0, 0, 0, 0);
         Mockito.when(alienUserDao.find(Mockito.anyMap(), Mockito.eq(1))).thenReturn(searchResult);
         userService.ensureAdminUser();
 
         Mockito.verify(alienUserDao, Mockito.times(1)).save(Mockito.any(User.class));
+    }
+
+    @After
+    public void cleanup() {
+        Mockito.reset(alienUserDao);
     }
 }
