e6663c6eacb58692c4f89ee647e885fafa2693a7
Xavier DEGENNE <xavier.degenne@gmail.com>
2020-01-29 14:44:16 +0100

    [ALIEN-3347] We need to eventually define a backUrl

--- alien4cloud-tosca/src/main/java/alien4cloud/topology/task/RedirectionTask.java
+++ alien4cloud-tosca/src/main/java/alien4cloud/topology/task/RedirectionTask.java
@@ -9,11 +9,25 @@ import lombok.Setter;
 @NoArgsConstructor
 public class RedirectionTask extends AbstractTask {
 
+    /**
+     * The URL to redirect to. This will be considered by the Wizard UI in order to achieve a redirection.
+     */
     private String url;
+
+    /**
+     * If not null, this param name will be added to the url with in value, the back URL (url to come back after the redirection).
+     */
+    private String backUrlParam;
 
     public RedirectionTask(String url) {
         setCode(TaskCode.REDIRECTION_REQUIRED);
         this.url = url;
+    }
+
+    public RedirectionTask(String url, String backUrlParam) {
+        setCode(TaskCode.REDIRECTION_REQUIRED);
+        this.url = url;
+        this.backUrlParam = backUrlParam;
     }
 
 }
