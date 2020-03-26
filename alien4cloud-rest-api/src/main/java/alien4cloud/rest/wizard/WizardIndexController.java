5631721aafdf4541ecbedbb3204ae6fc7f962d59
Xavier DEGENNE <xavier.degenne@gmail.com>
2020-03-26 10:12:30 +0100

    [ALIEN-3415] try a JSP approach for wizard index (WIP)

---
+++ alien4cloud-rest-api/src/main/java/alien4cloud/rest/wizard/WizardIndexController.java
@@ -0,0 +1,20 @@
+package alien4cloud.rest.wizard;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class WizardIndexController {
+
+    @RequestMapping(value = { "/wizard" }, method = RequestMethod.GET)
+    public String index(Model model) {
+
+        String base_href = "/toto";
+        model.addAttribute("base_href", base_href);
+
+        return "wizard";
+    }
+
+}
