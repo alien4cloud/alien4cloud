3ecd560409194aaf321d1ce90d25b79a25bb07d6
Xavier DEGENNE <xavier.degenne@gmail.com>
2020-05-13 18:02:21 +0200

    [ALIEN-3451] we now use Thymeleaf + nekohtml  to generate a wizard.html aware of context path

---
+++ alien4cloud-rest-api/src/main/java/alien4cloud/webconfiguration/WebTemplatingConfiguration.java
@@ -0,0 +1,63 @@
+package alien4cloud.webconfiguration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Description;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+/**
+ * The Thymeleaf templating engine is just used to manager wizard html page.
+ */
+@Configuration
+public class WebTemplatingConfiguration {
+
+    @Bean
+    @Description("Thymeleaf template resolver serving HTML 5")
+    public ClassLoaderTemplateResolver templateResolver() {
+
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+
+        templateResolver.setPrefix("wizard/");
+        templateResolver.setCacheable(false);
+        templateResolver.setSuffix(".html");
+        // LEGACYHTML5 since webpack generated malformated html
+        templateResolver.setTemplateMode("LEGACYHTML5");
+        templateResolver.setCharacterEncoding("UTF-8");
+
+        return templateResolver;
+    }
+
+    @Bean
+    @Description("Thymeleaf template engine with Spring integration")
+    public SpringTemplateEngine templateEngine() {
+
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+
+        return templateEngine;
+    }
+
+    @Bean
+    @Description("Thymeleaf view resolver")
+    public ViewResolver viewResolver() {
+
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+
+        viewResolver.setTemplateEngine(templateEngine());
+        viewResolver.setCharacterEncoding("UTF-8");
+
+        return viewResolver;
+    }
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/wizard").setViewName("wizard");
+    }
+
+
+}
