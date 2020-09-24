b22d23a2f562f42aac4b0b59f4174d023b5edc28
Xavier DEGENNE <xavier.degenne@gmail.com>
2020-09-24 12:16:26 +0200

    [ALIEN-3517] Better error handling when no addons are found

--- alien4cloud-rest-api/src/main/java/alien4cloud/webconfiguration/WizardAddonsScanner.java
+++ alien4cloud-rest-api/src/main/java/alien4cloud/webconfiguration/WizardAddonsScanner.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 import javax.annotation.PostConstruct;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Map;
 import java.util.Set;
@@ -64,6 +65,8 @@ public class WizardAddonsScanner {
                     log.error("Not able to parse wizard_addon.json, addon will be ignored", parseException);
                 }
             }
+        } catch(FileNotFoundException fnfe) {
+            log.info("No wizard addons found");
         } catch(IOException ioe) {
             log.error("Exception while exploring wizard_addons folder in classpath", ioe);
         }
